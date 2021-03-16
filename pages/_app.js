9d4bd93664120c4a06523989c77daf517f93122b
Renato Pozzi <r.pozzi@bluecube.it>
2021-03-16 22:50:07 +0100

    Added functions in env

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
-import '../styles/globals.css'
-
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
