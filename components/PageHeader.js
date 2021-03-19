de95d28fc20ee8c6e60981c80ffb2a214ee65775
Renato Pozzi <r.pozzi@bluecube.it>
2021-03-20 00:07:35 +0100

    Added APIs

--- components/PageHeader.js
+++ components/PageHeader.js
@@ -1,37 +1,39 @@
-const PageHeader = () => (
-  <div className="bg-white">
-    <div className="px-4 sm:px-6 lg:mx-auto lg:px-8">
-      <div className="py-6 md:flex md:items-center md:justify-between lg:border-t lg:border-gray-200">
-        <div className="flex-1 min-w-0">
-          <div className="flex items-center">
-            <div>
-              <div className="flex items-center">
-                <h1 className="text-2xl font-bold leading-7 text-gray-900 sm:leading-9 sm:truncate">
-                  animesplash.tv
-                </h1>
+const PageHeader = () => {
+  return (
+    <div className="bg-white">
+      <div className="px-4 sm:px-6 lg:mx-auto lg:px-8">
+        <div className="py-6 md:flex md:items-center md:justify-between lg:border-t lg:border-gray-200">
+          <div className="flex-1 min-w-0">
+            <div className="flex items-center">
+              <div>
+                <div className="flex items-center">
+                  <h1 className="text-2xl font-bold leading-7 text-gray-900 sm:leading-9 sm:truncate">
+                    animesplash.tv
+                  </h1>
+                </div>
+                <dl className="mt-6 flex flex-col sm:mt-1 sm:flex-row sm:flex-wrap">
+                  <dd className="flex items-center text-sm text-gray-500 font-medium sm:mr-6 sm:mt-0 capitalize">
+                    <span className="flex relative h-3 w-3 mr-2">
+                      <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-indigo-400 opacity-75"></span>
+                      <span className="relative inline-flex rounded-full h-3 w-3 bg-indigo-500"></span>
+                    </span>
+                    12 Current Visitors
+                  </dd>
+                </dl>
               </div>
-              <dl className="mt-6 flex flex-col sm:mt-1 sm:flex-row sm:flex-wrap">
-                <dd className="flex items-center text-sm text-gray-500 font-medium sm:mr-6 sm:mt-0 capitalize">
-                  <span className="flex relative h-3 w-3 mr-2">
-                    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-indigo-400 opacity-75"></span>
-                    <span className="relative inline-flex rounded-full h-3 w-3 bg-indigo-500"></span>
-                  </span>
-                  12 Current Visitors
-                </dd>
-              </dl>
             </div>
           </div>
-        </div>
-        <div className="mt-6 flex space-x-3 md:mt-0 md:ml-4">
-          <button
-            type="button"
-            className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-            Reload
-          </button>
+          <div className="mt-6 flex space-x-3 md:mt-0 md:ml-4">
+            <button
+              type="button"
+              className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              Reload
+            </button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default PageHeader;
---
+++ pages/api/metrics/page-views.js
@@ -0,0 +1,24 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+module.exports = async (req, res) => {
+  // Only GET Available
+  if (req.method !== "GET") {
+    return res.status(405).json({ message: "Method not allowed." });
+  }
+
+  const { type, element } = req.body;
+
+  // All the pages viewed
+  const allPageWithViews = await prisma.$queryRaw`
+    SELECT element, count(element) as views
+    FROM "Event"
+    GROUP BY element
+    ORDER BY views DESC
+  `;
+
+  return res.json({
+    data: allPageWithViews,
+  });
+};
