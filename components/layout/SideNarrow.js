1f707f7659c1938f02f25079cc3938851b232151
Renato Pozzi <r.pozzi@bluecube.it>
2021-03-20 18:20:51 +0100

    Some improvements

--- components/layout/SideNarrow.js
+++ components/layout/SideNarrow.js
@@ -16,9 +16,9 @@ const SideNarrow = () => (
           stroke="currentColor"
           aria-hidden="true">
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
           />
         </svg>
@@ -37,9 +37,9 @@ const SideNarrow = () => (
           stroke="currentColor"
           aria-hidden="true">
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
           />
         </svg>
@@ -58,9 +58,9 @@ const SideNarrow = () => (
           stroke="currentColor"
           aria-hidden="true">
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
           />
         </svg>
@@ -79,9 +79,9 @@ const SideNarrow = () => (
           stroke="currentColor"
           aria-hidden="true">
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             d="M3 21v-4m0 0V5a2 2 0 012-2h6.5l1 1H21l-3 6 3 6h-8.5l-1-1H5a2 2 0 00-2 2zm9-13.5V9"
           />
         </svg>
@@ -100,9 +100,9 @@ const SideNarrow = () => (
           stroke="currentColor"
           aria-hidden="true">
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"
           />
         </svg>
@@ -121,9 +121,9 @@ const SideNarrow = () => (
           stroke="currentColor"
           aria-hidden="true">
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
           />
         </svg>
---
+++ pages/api/metrics/views/series.js
@@ -0,0 +1,109 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+const thisRangeViews = async (range = "month") => {
+  console.log("RANGE", range);
+  return await prisma.$queryRaw`
+    SELECT
+      range.generate_series as range,
+      COALESCE(e.views, 0) AS views
+    FROM
+      (
+        select
+          generate_series(
+            date_trunc(${range}, now()),
+            date_trunc(${range}, now()) + ${`1 ${range}`} :: interval - '1 day' :: interval,
+            '1 day' :: interval
+          ):: date
+      ) as range
+      LEFT JOIN (
+        SELECT
+          "createdAt" :: date AS day,
+          Count(id) AS views
+        FROM
+          "Event"
+        GROUP BY
+          day
+      ) AS e ON range.generate_series = e.day
+    ORDER BY
+      range
+  `;
+};
+
+const thisDayViews = async () =>
+  await prisma.$queryRaw`
+    SELECT
+      range,
+      COALESCE(e.views, 0) AS views
+    FROM
+      Generate_series(0, 23) AS range
+      LEFT JOIN (
+        SELECT
+          Date_part('hour', "createdAt") AS hour,
+          Count(id) AS views
+        FROM
+          "Event"
+        WHERE
+          "createdAt" >= now() :: date
+        GROUP BY
+          hour
+      ) AS e ON range = e.hour
+    ORDER BY
+      range
+  `;
+
+module.exports = async (req, res) => {
+  // Only GET Available
+  if (req.method !== "GET") {
+    return res.status(405).json({ message: "Method not allowed." });
+  }
+
+  const { range } = req.query;
+
+  let data = {};
+
+  if (range === "this_day") {
+    data = await thisDayViews()
+      .catch((e) => {
+        throw e;
+      })
+      .finally(async () => {
+        await prisma.$disconnect();
+      });
+  } else if (range === "this_year") {
+    data = await thisRangeViews("year")
+      .catch((e) => {
+        throw e;
+      })
+      .finally(async () => {
+        await prisma.$disconnect();
+      });
+  } else if (range === "this_month") {
+    data = await thisRangeViews("month")
+      .catch((e) => {
+        throw e;
+      })
+      .finally(async () => {
+        await prisma.$disconnect();
+      });
+  } else if (range === "this_week") {
+    data = await thisRangeViews("week")
+      .catch((e) => {
+        throw e;
+      })
+      .finally(async () => {
+        await prisma.$disconnect();
+      });
+  } else {
+    data = [];
+  }
+
+  //console.log("data", data);
+  // format data
+  const x = data.map((dv) => [dv.range, dv.views]);
+
+  return res.json({
+    data: x,
+  });
+};
