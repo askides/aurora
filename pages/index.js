4810246518799a7dd2a5954b646e254256f36294
Renato Pozzi <r.pozzi@bluecube.it>
2021-03-20 16:23:15 +0100

    Some improvements

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import Header from "../components/layout/Header";
 import PageHeader from "../components/PageHeader";
 import SimpleChart from "../components/SimpleChart";
 import ListView from "../components/ListView";
+import Stats from "../components/Stats";
+import Tile from "../components/Tile";
 
 export default function Home() {
   return (
@@ -13,20 +15,36 @@ export default function Home() {
       <div class="flex flex-col min-w-0 flex-1 overflow-hidden">
         {/*  Main area  */}
         <PageHeader />
-        <div class="flex-1 relative z-0 flex overflow-hidden">
+        <div class="flex-1 relative z-0 flex">
           <main
             class="flex-1 relative z-0 overflow-y-auto focus:outline-none xl:order-last"
             tabindex="0">
             <div class="absolute inset-0 py-6 px-4 sm:px-6 lg:px-8">
-              <div class="h-full border-2 border-gray-200 border-dashed rounded-lg">
+              <div class="h-full  rounded-lg">
                 <SimpleChart />
+
+                <div className="grid grid-cols-3 grid-rows-3 gap-4">
+                  <Tile>
+                    <div className="flex flex-col text-center">
+                      <div>5890</div>
+                      <div>iPhone</div>
+                    </div>
+                  </Tile>
+                  <Tile>
+                    <div className="flex flex-col text-center">
+                      <div>1456</div>
+                      <div>Chrome</div>
+                    </div>
+                  </Tile>
+                </div>
               </div>
             </div>
           </main>
 
-          <aside class="hidden relative xl:order-first xl:flex xl:flex-col flex-shrink-0 w-96 border-r border-gray-200">
+          <aside class="overflow-y-auto hidden relative xl:order-first xl:flex xl:flex-col flex-shrink-0 w-96 border-r border-gray-200">
             <div class="absolute inset-0 py-6 px-4 sm:px-6 lg:px-8">
               <div class="h-full border-2 border-gray-200 border-dashed rounded-lg space-y-4">
+                <Stats />
                 <ListView />
                 <ListView />
                 <ListView />
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ module.exports = {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      transform: ["hover"],
+      borderWidth: ["hover"],
+    },
   },
   plugins: [require("@tailwindcss/forms")],
 };
