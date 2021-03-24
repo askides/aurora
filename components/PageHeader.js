7301aa04807a663084c75d4cc61a2b5933f68e29
Renato Pozzi <r.pozzi@bluecube.it>
2021-03-25 00:45:23 +0100

    Implemented current visitors

--- components/PageHeader.js
+++ components/PageHeader.js
@@ -1,11 +1,29 @@
+import useSWR from "swr";
+
 const PageHeader = ({ timeRange, onTimeRangeChange }) => {
+  const fetcher = (...args) =>
+    fetch(...args)
+      .then((res) => res.json())
+      .then((res) => res.data);
+
+  const { data, error } = useSWR("/api/metrics/realtime/visitors", fetcher);
+
   return (
     <div className="pt-10">
       <div className="mt-2 md:flex md:items-center md:justify-between">
-        <div className="flex-1 min-w-0">
+        <div className="flex-1 min-w-0 space-y-2">
           <h2 className="text-2xl font-bold leading-7 text-gray-900 dark:text-white sm:text-3xl sm:truncate">
             animesplash.tv
           </h2>
+          <div className="flex items-center">
+            <span class="mr-3 flex relative h-3 w-3">
+              <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
+              <span class="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
+            </span>
+            <div className="text-white text-sm">
+              Current Visitors: {!error && data ? data.visitors : 0}
+            </div>
+          </div>
         </div>
         <div className="mt-4 flex-shrink-0 flex md:mt-0 md:ml-4">
           <select
