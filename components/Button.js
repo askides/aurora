7750e39f70e6aef4869c049ed95d13c56685ee7c
Renato Pozzi <r.pozzi@bluecube.it>
2021-03-25 17:35:56 +0100

    Added login page

---
+++ components/Button.js
@@ -0,0 +1,31 @@
+const Button = ({ label, type, isLoading }) => {
+  return (
+    <button
+      type={type}
+      disabled={isLoading}
+      className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+      {isLoading && (
+        <svg
+          className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24">
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            stroke-width="4"></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        </svg>
+      )}
+      {label}
+    </button>
+  );
+};
+
+export default Button;
