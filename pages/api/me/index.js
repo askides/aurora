8c0d15484f46978bb4deb12247652416f463e2ac
Renato Pozzi <r.pozzi@bluecube.it>
2021-03-25 15:05:13 +0100

    Improvements

--- pages/api/me/index.js
+++ pages/api/me/index.js
@@ -6,16 +6,8 @@ const handle = async function (req, res) {
     return res.status(405).json({ message: "Method not allowed." });
   }
 
-  // Retrieve Data
-  const user = req.accessTokenBody.data;
-
-  if (user) {
-    // Return User Data.
-    return res.status(200).json({ data: user });
-  }
-
-  // User false, so not found.
-  return res.status(404).json({ message: "Not found" });
+  // Return User Data.
+  return res.status(200).json({ data: req.accessTokenBody.data });
 };
 
 module.exports = withAuth(handle);
