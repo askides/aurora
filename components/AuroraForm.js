57de82de18147f2a15e059890cd44725530a42c5
Renato Pozzi <r.pozzi@bluecube.it>
2021-03-26 21:51:01 +0100

    Moved components

--- components/AuroraForm.js
+++ components/AuroraForm.js
@@ -25,7 +25,7 @@ export const Button = ({ label, type, isLoading }) => {
     <button
       type={type}
       disabled={isLoading}
-      className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-gray-700 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900">
+      className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-green-500">
       {isLoading && <SvgLoader />}
       {label}
     </button>
