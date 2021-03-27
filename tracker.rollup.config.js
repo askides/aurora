a5c90c17cd325b4ca0f30c4165d1fd67c5606fdb
Renato Pozzi <r.pozzi@bluecube.it>
2021-03-27 23:31:13 +0100

    Added runtimegenerator

--- tracker.rollup.config.js
+++
@@ -1,9 +0,0 @@
-import { terser } from "rollup-plugin-terser";
-export default {
-  input: "tracker/aurora.js",
-  output: {
-    file: "public/aurora.js",
-    format: "cjs",
-    plugins: [terser()],
-  },
-};
