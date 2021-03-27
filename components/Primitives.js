6a116be3c0e21893571bd3fd5833dc15fdbbe7e9
Renato Pozzi <r.pozzi@bluecube.it>
2021-03-27 15:41:03 +0100

    Added buttons

--- components/Primitives.js
+++ components/Primitives.js
@@ -9,12 +9,17 @@ export const StackedList = ({ children }) => (
   <ul className="divide-y divide-gray-200 dark:divide-gray-700">{children}</ul>
 );
 
-export const StackedListItem = ({ avatar, title, subtitle }) => (
-  <li className="pt-4 pb-4 first:pt-0 last:pb-0 flex">
-    <img className="h-10 w-10 rounded-full" src={avatar} alt="" />
-    <div className="ml-3">
-      <p className="text-sm font-medium text-gray-900 dark:text-white">{title}</p>
-      <p className="text-sm text-gray-500 dark:text-gray-300">{subtitle}</p>
+export const StackedListItem = ({ avatar, title, subtitle, actions }) => (
+  <li className="py-4 first:pt-0 last:pb-0">
+    <div className="flex items-center space-x-4">
+      <div className="flex-shrink-0">
+        <img className="h-8 w-8 rounded-full" src={avatar} alt="" />
+      </div>
+      <div className="flex-1 min-w-0">
+        <p className="text-sm font-medium text-gray-900 dark:text-white truncate">{title}</p>
+        <p className="text-sm text-gray-500 dark:text-gray-300 truncate">{subtitle}</p>
+      </div>
+      {actions}
     </div>
   </li>
 );
