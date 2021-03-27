0379ab016c1be294e250cfa2072d0478430a661a
Renato Pozzi <r.pozzi@bluecube.it>
2021-03-27 15:41:03 +0100

    Added buttons

--- pages/websites/index.js
+++ pages/websites/index.js
@@ -31,6 +31,20 @@ const Websites = () => {
                 </Link>
               }
               subtitle={el.url}
+              actions={
+                <div className="space-x-2">
+                  <Link href={`/websites/${el.seed}/edit`}>
+                    <a className="inline-flex items-center shadow-sm px-2.5 py-0.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50">
+                      Edit
+                    </a>
+                  </Link>
+                  <Link href={`/websites/${el.seed}`}>
+                    <a className="inline-flex items-center shadow-sm px-2.5 py-0.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50">
+                      VIew Dashboard
+                    </a>
+                  </Link>
+                </div>
+              }
             />
           ))}
         </StackedList>
