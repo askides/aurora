ad4c3bd88904204488ec056fc6e0b171982e1763
Renato Pozzi <r.pozzi@bluecube.it>
2021-03-27 15:32:30 +0100

    Moved to websites path

--- components/layout/SideNarrow.js
+++ components/layout/SideNarrow.js
@@ -19,26 +19,6 @@ const SideNarrow = () => (
     aria-label="Sidebar"
     className="hidden md:block md:flex-shrink-0 md:bg-gray-800 md:overflow-y-auto">
     <div className="relative w-20 flex flex-col p-3 space-y-3">
-      <SideNarrowItem href="/">
-        <a className="text-gray-400 hover:bg-gray-700 flex-shrink-0 inline-flex items-center justify-center h-14 w-14 rounded-lg">
-          {/*  Heroicon name: outline/inbox  */}
-          <svg
-            className="h-6 w-6"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            aria-hidden="true">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
-            />
-          </svg>
-        </a>
-      </SideNarrowItem>
-
       <SideNarrowItem href="/websites">
         <a className="text-gray-400 hover:bg-gray-700 flex-shrink-0 inline-flex items-center justify-center h-14 w-14 rounded-lg">
           <span className="sr-only">Flagged</span>
