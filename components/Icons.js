4a9855882b7f26de7a51bf571e645b65b355b192
Renato Pozzi <r.pozzi@bluecube.it>
2021-03-28 21:33:36 +0200

    Moved arrows

---
+++ components/Icons.js
@@ -0,0 +1,25 @@
+export const ArrowUp = () => (
+  <svg
+    className="-ml-1 mr-0.5 flex-shrink-0 self-center h-5 w-5 text-green-500"
+    fill="currentColor"
+    viewBox="0 0 20 20"
+    aria-hidden="true">
+    <path
+      fill-rule="evenodd"
+      d="M5.293 9.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 7.414V15a1 1 0 11-2 0V7.414L6.707 9.707a1 1 0 01-1.414 0z"
+      clip-rule="evenodd"></path>
+  </svg>
+);
+
+export const ArrowDown = () => (
+  <svg
+    className="-ml-1 mr-0.5 flex-shrink-0 self-center h-5 w-5 text-red-500"
+    fill="currentColor"
+    viewBox="0 0 20 20"
+    aria-hidden="true">
+    <path
+      fill-rule="evenodd"
+      d="M14.707 10.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 12.586V5a1 1 0 012 0v7.586l2.293-2.293a1 1 0 011.414 0z"
+      clip-rule="evenodd"></path>
+  </svg>
+);
