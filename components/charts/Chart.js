27de535d71e2cd46cd1e10e0a1e603df6f040675
Renato Pozzi <r.pozzi@bluecube.it>
2021-03-28 22:47:55 +0200

    Improved UX

--- components/charts/Chart.js
+++ components/charts/Chart.js
@@ -17,26 +17,15 @@ const Chart = ({ url, timeRange, title = "Chart", type = "columnChart" }) => {
   if (isError) return <div>failed to load</div>;
 
   return (
-    <div className="bg-white dark:bg-gray-800 w-full overflow-hidden shadow rounded-lg divide-y divide-gray-200 dark:divide-gray-700">
-      <div className="px-4 py-5 sm:px-6">
-        <div className="-ml-4 -mt-4 flex justify-between items-center flex-wrap sm:flex-nowrap">
-          <div className="ml-4 mt-4">
-            <h3 className="text-xs font-semibold text-gray-500 uppercase tracking-wider dark:text-green-400">
-              {title}
-            </h3>
-          </div>
-          <div className="ml-4 mt-4 flex-shrink-0"></div>
-        </div>
-      </div>
+    <div className="bg-white dark:bg-gray-800 w-full overflow-hidden shadow rounded-b-lg divide-y divide-gray-200 dark:divide-gray-700">
       <div className="px-4 py-5 sm:p-6">
         {React.createElement(Components[type], {
           data: graph,
-          //colors: ["#66c48170", "#666"],
           dataset: {
             backgroundColor: "rgba(147, 197, 253, 0.1)",
             borderWidth: 3,
             lineTension: 0.2,
-            //borderDash: [5, 10],
+            // borderDash: [5, 5],
             fill: true,
           },
           curve: false,
