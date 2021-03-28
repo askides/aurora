c4e7ec58ee4a9196e635bdf5e0a7be5ba451a8f9
Renato Pozzi <r.pozzi@bluecube.it>
2021-03-28 15:14:06 +0200

    Fixed padding

--- components/layout/MainLayout.js
+++ components/layout/MainLayout.js
@@ -7,7 +7,7 @@ const MainLayout = ({ children }) => (
     <div className="flex min-w-0 flex-1 overflow-y-auto justify-center">
       <div className="flex-1 relative z-0 flex max-w-5xl">
         <main className="flex-1 relative z-0 focus:outline-none xl:order-last" tabIndex="0">
-          <div className="absolute inset-0 py-6 px-4 sm:px-6 lg:px-8">
+          <div className="inset-0 py-6 px-4 sm:px-6 lg:px-8">
             <Header />
             {children}
           </div>
