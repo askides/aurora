34d069936e54b4374d59fb904ddb72ca634f6918
Renato Pozzi <r.pozzi@bluecube.it>
2021-03-28 22:04:01 +0200

    Refactoring Page Header

---
+++ components/WebsiteCurrentVisitors.js
@@ -0,0 +1,25 @@
+import useSWR from "swr";
+
+const WebsiteCurrentVisitors = ({ seed }) => {
+  const fetcher = (...args) =>
+    fetch(...args)
+      .then((res) => res.json())
+      .then((res) => res.data);
+
+  const { data, error } = useSWR(`/api/metrics/${seed}/realtime/visitors`, fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+
+  return (
+    <div className="flex items-center">
+      <span className="mr-3 flex relative h-3 w-3">
+        <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-blue-400 opacity-75"></span>
+        <span className="relative inline-flex rounded-full h-3 w-3 bg-blue-500"></span>
+      </span>
+      <div className="text-white text-sm">Current Visitors: {data.visitors}</div>
+    </div>
+  );
+};
+
+export default WebsiteCurrentVisitors;
--- pages/websites/[seed]/index.js
+++ pages/websites/[seed]/index.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import WebsiteInfo from "../../../components/WebsiteInfo";
 import Chart from "../../../components/charts/Chart";
 import Performance from "../../../components/charts/Performance";
 import { withAuth } from "../../../components/utils/withAuth";
@@ -7,6 +6,9 @@ import BrowserViews from "../../../components/charts/BrowserViews";
 import OsViews from "../../../components/charts/OsViews";
 import PageViews from "../../../components/charts/PageViews";
 import CountryViews from "../../../components/charts/CountryViews";
+import WebsiteName from "../../../components/WebsiteName";
+import WebsiteCurrentVisitors from "../../../components/WebsiteCurrentVisitors";
+import RangeSelector from "../../../components/RangeSelector";
 
 export async function getServerSideProps(context) {
   const { seed } = context.query;
@@ -21,17 +23,27 @@ const Website = ({ seed }) => {
 
   return (
     <div className="h-full rounded-lg space-y-4 bg-gray-900">
-      <WebsiteInfo
-        url={`/api/metrics/${seed}/realtime/visitors`}
-        onTimeRangeChange={(value) => setTimeRange(value)}
-      />
+      <div className="pt-10">
+        <div className="mt-2 md:flex md:items-center md:justify-between">
+          <div className="flex-1 min-w-0 space-y-2">
+            <WebsiteName seed={seed} />
+            <WebsiteCurrentVisitors seed={seed} />
+          </div>
+          <div className="mt-4 flex-shrink-0 flex md:mt-0 md:ml-4">
+            <RangeSelector onTimeRangeChange={(value) => setTimeRange(value)} />
+          </div>
+        </div>
+      </div>
+
       <Performance url={`/api/metrics/${seed}/performance`} timeRange={timeRange} />
+
       <Chart
         url={`/api/metrics/${seed}/views/series`}
         timeRange={timeRange}
         title="Page Views"
         type="areaChart"
       />
+
       <PageViews url={`/api/metrics/${seed}/views/pages`} timeRange={timeRange} />
       <OsViews url={`/api/metrics/${seed}/views/oses`} timeRange={timeRange} />
       <BrowserViews url={`/api/metrics/${seed}/views/browsers`} timeRange={timeRange} />
