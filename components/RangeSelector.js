1a4b2aeb9c3c0efcd7ba61f1ab9059e3dfc238ee
Renato Pozzi <r.pozzi@bluecube.it>
2021-04-01 19:39:59 +0200

    Changed range values

--- components/RangeSelector.js
+++ components/RangeSelector.js
@@ -3,12 +3,12 @@ const RangeSelector = ({ timeRange, onTimeRangeChange }) => (
     value={timeRange}
     onChange={(e) => onTimeRangeChange(e.target.value)}
     className="mt-1 block w-full pl-3 pr-10 py-2 text-base dark:text-white cursor-pointer font-medium border-gray-300 dark:border-gray-800 dark:bg-gray-800 focus:outline-none focus:ring-indigo-500 dark:focus:ring-green-500 focus:border-indigo-500 dark:focus:border-green-500 sm:text-sm rounded-md">
-    <option defaultValue="this_day" value="this_day">
+    <option defaultValue="day" value="day">
       Today
     </option>
-    <option value="this_week">This Week</option>
-    <option value="this_month">This Month</option>
-    <option value="this_year">This Year</option>
+    <option value="week">This Week</option>
+    <option value="month">This Month</option>
+    <option value="year">This Year</option>
   </select>
 );
 
