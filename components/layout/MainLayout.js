03ddc3fbf9acea525b5e7937d7fa005edd607803
Renato Pozzi <r.pozzi@bluecube.it>
2021-04-02 19:29:35 +0200

    Some fix in css

--- components/layout/MainLayout.js
+++ components/layout/MainLayout.js
@@ -7,10 +7,8 @@ const MainLayout = ({ children }) => (
     <div className="flex min-w-0 flex-1 overflow-y-auto justify-center">
       <div className="flex-1 relative z-0 flex max-w-5xl">
         <main className="flex-1 relative z-0 focus:outline-none xl:order-last" tabIndex="0">
-          <div className="inset-0 py-6 px-4 sm:px-6 lg:px-8">
-            <Header />
-            {children}
-          </div>
+          <Header />
+          <div className="inset-0 py-6 px-4 sm:px-6 lg:px-8">{children}</div>
         </main>
       </div>
     </div>
