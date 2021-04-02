e8115cb676b77d3615df083a979fd32b68689ffc
Renato Pozzi <r.pozzi@bluecube.it>
2021-04-02 17:34:20 +0200

    Fixed bad css

--- components/Icons.js
+++ components/Icons.js
@@ -5,9 +5,9 @@ export const ArrowUp = () => (
     viewBox="0 0 20 20"
     aria-hidden="true">
     <path
-      fill-rule="evenodd"
+      fillRule="evenodd"
       d="M5.293 9.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 7.414V15a1 1 0 11-2 0V7.414L6.707 9.707a1 1 0 01-1.414 0z"
-      clip-rule="evenodd"></path>
+      clipRule="evenodd"></path>
   </svg>
 );
 
@@ -18,8 +18,8 @@ export const ArrowDown = () => (
     viewBox="0 0 20 20"
     aria-hidden="true">
     <path
-      fill-rule="evenodd"
+      fillRule="evenodd"
       d="M14.707 10.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 12.586V5a1 1 0 012 0v7.586l2.293-2.293a1 1 0 011.414 0z"
-      clip-rule="evenodd"></path>
+      clipRule="evenodd"></path>
   </svg>
 );
