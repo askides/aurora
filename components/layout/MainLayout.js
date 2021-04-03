a3bb208113fa6261fe8863922ecccb5662afe58c
Renato Pozzi <r.pozzi@bluecube.it>
2021-04-03 23:40:12 +0200

    Added shared layout

--- components/layout/MainLayout.js
+++ components/layout/MainLayout.js
@@ -1,9 +1,9 @@
 import SideNarrow from "./SideNarrow";
 import Header from "./Header";
 
-const MainLayout = ({ children }) => (
+const MainLayout = ({ children, sideNarrow }) => (
   <div className="sm:h-screen flex overflow-hidden bg-gray-50 dark:bg-gray-900">
-    <SideNarrow />
+    {sideNarrow && <SideNarrow />}
     <div className="flex min-w-0 flex-1 overflow-y-auto justify-center">
       <div className="flex-1 relative z-0 flex max-w-5xl">
         <main className="flex-1 relative z-0 focus:outline-none xl:order-last" tabIndex="0">
