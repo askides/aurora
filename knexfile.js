741bf83b82207e4f1ccd3963efaf583d4cdf4a66
Renato Pozzi <r.pozzi@bluecube.it>
2021-04-05 23:16:16 +0200

    Updated config

--- knexfile.js
+++ knexfile.js
@@ -3,13 +3,7 @@ require("dotenv").config();
 module.exports = {
   production: {
     client: "pg",
-    connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      database: process.env.DB_DATABASE,
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-    },
+    connection: process.env.DB_URL,
     pool: {
       min: 2,
       max: 10,
