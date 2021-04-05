ef7b65a94c6678ee5bb3bd535872dbd90e35f4fa
Renato Pozzi <r.pozzi@bluecube.it>
2021-04-05 22:50:55 +0200

    FInalized configurations

--- knexfile.js
+++ knexfile.js
@@ -1,28 +1,14 @@
-// Update with your config settings.
+require("dotenv").config();
 
 module.exports = {
-  development: {
+  production: {
     client: "pg",
     connection: {
-      database: "aurora",
-      user: "root",
-      password: "password",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-    },
-  },
-
-  production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_DATABASE,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
     },
     pool: {
       min: 2,
