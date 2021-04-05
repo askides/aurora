5768df541242079dff83d093d657e599a5060cba
Renato Pozzi <r.pozzi@bluecube.it>
2021-04-05 20:44:38 +0200

    Removed prisma & dependencies

--- prisma/migrations/20210319170915_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `element` to the `Event` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Event" ADD COLUMN     "element" TEXT NOT NULL;
--- prisma/migrations/20210320212259_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `hash` to the `Event` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Event" ADD COLUMN     "hash" TEXT NOT NULL;
