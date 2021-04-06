7e67de29fc40eafd09c0bc7ae3e1afc03ebe3d85
Renato Pozzi <r.pozzi@bluecube.it>
2021-04-06 11:40:06 +0200

    Added development data

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,13 @@
 require("dotenv").config();
 
 module.exports = {
+  development: {
+    client: "pg",
+    connection: process.env.DB_URL,
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
   production: {
     client: "pg",
     connection: {
