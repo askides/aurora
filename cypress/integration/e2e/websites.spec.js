122ad49604fb089e3b64e23fafd569ce854293e3
Renato Pozzi <r.pozzi@bluecube.it>
2021-04-06 22:35:18 +0200

    Updated some tests

--- cypress/integration/e2e/websites.spec.js
+++ cypress/integration/e2e/websites.spec.js
@@ -1,14 +1,20 @@
-describe("Login & Websites Creation", () => {
-  it("go to login and type correct data.", () => {
-    cy.visit("/auth/login");
-    cy.get('[type="email"]').type("info@renatopozzi.me");
-    cy.get('[type="password"]').type("password");
-    cy.get('[type="submit"]').click();
-    cy.location("pathname").should("equal", "/");
+beforeEach(() => {
+  cy.exec("npm run db:test-seed");
+  cy.request({
+    method: "POST",
+    url: "/api/auth/login",
+    body: { email: "info@renatopozzi.me", password: "password" },
+  });
+});
 
+describe("Websites Creation", () => {
+  it("should create a website and edit", () => {
+    cy.visit("/");
     cy.contains("Create New").click();
     cy.location("pathname").should("equal", "/websites/create");
     cy.get('[name="url"]').type("https://marcopisellonio.net");
+    cy.get('[type="submit"]').click();
+    cy.get('[name="name"]').type("Goldissimo Bundi");
     cy.get('[type="submit"]').click();
   });
 });
