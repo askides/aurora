ccf6c9b14cd6dae60911c55f28c0a157a6a7ff9f
Renato Pozzi <r.pozzi@bluecube.it>
2021-04-07 18:54:04 +0200

    Added unit testing

---
+++ cypress/integration/api/metrics/[seed]/views/oses.spec.js
@@ -0,0 +1,18 @@
+beforeEach(() => {
+  cy.exec("npm run db:test-seed");
+});
+
+describe("Views API", () => {
+  const seed = "40551333ba09839f5287a7a6aa2f73fe";
+
+  it("Not Authenticated.", () => {
+    cy.request({
+      method: "GET",
+      url: `/api/metrics/${seed}/views/oses`,
+      failOnStatusCode: false,
+    }).then((resp) => {
+      expect(resp.status).to.eq(401);
+      expect(resp.body).to.eql({ message: "Unauthorized" });
+    });
+  });
+});
