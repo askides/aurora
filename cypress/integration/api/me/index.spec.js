1c28daa860992b4a5c63711740dcc91479088b0c
Renato Pozzi <r.pozzi@bluecube.it>
2021-04-07 23:49:59 +0200

    Some testing

--- cypress/integration/api/me/index.spec.js
+++ cypress/integration/api/me/index.spec.js
@@ -1,3 +1,7 @@
+beforeEach(() => {
+  cy.exec("npm run db:test-seed");
+});
+
 describe("Profile API", () => {
   it("Not Authenticated.", () => {
     cy.request({
@@ -8,5 +12,10 @@ describe("Profile API", () => {
       expect(resp.status).to.eq(401);
       expect(resp.body).to.eql({ message: "Unauthorized" });
     });
+  });
+
+  it("Authenticated.", () => {
+    cy.login("info@renatopozzi.me", "password");
+    cy.request("/api/me");
   });
 });
--- cypress/integration/api/metrics/[seed]/views/oses.spec.js
+++ cypress/integration/api/metrics/[seed]/views/oses.spec.js
@@ -15,4 +15,24 @@ describe("Views API", () => {
       expect(resp.body).to.eql({ message: "Unauthorized" });
     });
   });
+
+  it("Authenticated Range Day.", () => {
+    cy.login("info@renatopozzi.me", "password");
+
+    cy.request({
+      method: "GET",
+      url: `/api/metrics/${seed}/views/oses?range=day`,
+      failOnStatusCode: false,
+    }).then((resp) => {
+      expect(resp.status).to.eq(200);
+      expect(resp.body).to.eql({
+        data: [
+          { element: "Windows", views: 9, unique: 3, percentage: 40.909090909090914 },
+          { element: "iOS", views: 6, unique: 1, percentage: 27.27272727272727 },
+          { element: "Mac OS", views: 4, unique: 1, percentage: 18.181818181818183 },
+          { element: "Windows Phone", views: 3, unique: 1, percentage: 13.636363636363635 },
+        ],
+      });
+    });
+  });
 });
