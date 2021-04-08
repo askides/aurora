44c0c59ca72c04b7a64254eb7bc61eb917dc56c5
Renato Pozzi <r.pozzi@bluecube.it>
2021-04-08 17:25:40 +0200

    Moved Aurora Loader SVG to Icons

--- components/AuroraForm.js
+++ components/AuroraForm.js
@@ -1,28 +1,6 @@
 import Link from "next/link";
 import { useField } from "formik";
-
-export const SvgLoader = () => (
-  <svg
-    className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-  >
-    <circle
-      className="opacity-25"
-      cx="12"
-      cy="12"
-      r="10"
-      stroke="currentColor"
-      strokeWidth="4"
-    ></circle>
-    <path
-      className="opacity-75"
-      fill="currentColor"
-      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-    ></path>
-  </svg>
-);
+import { Loader } from "./Icons";
 
 export const Button = ({ label, type, isLoading, href }) => {
   if (href) {
@@ -41,7 +19,7 @@ export const Button = ({ label, type, isLoading, href }) => {
       disabled={isLoading}
       className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-blue-500"
     >
-      {isLoading && <SvgLoader />}
+      {isLoading && <Loader />}
       {label}
     </button>
   );
--- components/Icons.js
+++ components/Icons.js
@@ -3,11 +3,13 @@ export const ArrowUp = () => (
     className="-ml-1 mr-0.5 flex-shrink-0 self-center h-5 w-5 text-green-500"
     fill="currentColor"
     viewBox="0 0 20 20"
-    aria-hidden="true">
+    aria-hidden="true"
+  >
     <path
       fillRule="evenodd"
       d="M5.293 9.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 7.414V15a1 1 0 11-2 0V7.414L6.707 9.707a1 1 0 01-1.414 0z"
-      clipRule="evenodd"></path>
+      clipRule="evenodd"
+    />
   </svg>
 );
 
@@ -16,10 +18,35 @@ export const ArrowDown = () => (
     className="-ml-1 mr-0.5 flex-shrink-0 self-center h-5 w-5 text-red-500"
     fill="currentColor"
     viewBox="0 0 20 20"
-    aria-hidden="true">
+    aria-hidden="true"
+  >
     <path
       fillRule="evenodd"
       d="M14.707 10.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 12.586V5a1 1 0 012 0v7.586l2.293-2.293a1 1 0 011.414 0z"
-      clipRule="evenodd"></path>
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+export const Loader = () => (
+  <svg
+    className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <circle
+      className="opacity-25"
+      cx="12"
+      cy="12"
+      r="10"
+      stroke="currentColor"
+      strokeWidth="4"
+    ></circle>
+    <path
+      className="opacity-75"
+      fill="currentColor"
+      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+    ></path>
   </svg>
 );
