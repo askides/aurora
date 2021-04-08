2f236fa1849b53c3c9e905f90866f2c97fd5019c
Renato Pozzi <r.pozzi@bluecube.it>
2021-04-08 17:17:36 +0200

    Removed useSWR

--- components/WebsiteName.js
+++ components/WebsiteName.js
@@ -1,20 +1,15 @@
-import useSWR from "swr";
-import clean from "../utils/url-cleaner";
+import { useWebsite } from "./hooks/useWebsite";
+import { dropProtocol } from "../utils/urls";
 
 const WebsiteName = ({ seed }) => {
-  const fetcher = (...args) =>
-    fetch(...args)
-      .then((res) => res.json())
-      .then((res) => res.data);
+  const { website, isLoading, isError } = useWebsite({ seed });
 
-  const { data, error } = useSWR(`/api/me/websites/${seed}`, fetcher);
-
-  if (error) return <div>failed to load</div>;
-  if (!data) return <div>loading...</div>;
+  if (isLoading) return <LoadingPanel />;
+  if (isError) return <div>failed to load</div>;
 
   return (
     <h2 className="text-2xl tracking-tight font-bold leading-7 text-gray-900 dark:text-white sm:text-3xl sm:truncate">
-      {clean(data.url)}
+      {dropProtocol(website.url)}
     </h2>
   );
 };
