a503db145a1c7d3a5805524e2c3fd223d77ad6da
Renato Pozzi <r.pozzi@bluecube.it>
2021-04-08 18:41:42 +0200

    Added initial toasts

--- pages/user/profile.js
+++ pages/user/profile.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { Formik, Form } from "formik";
+import { useToasts } from "react-toast-notifications";
 
 import { TextField, Select, Button } from "../../components/AuroraForm";
 import { Panel, LoadingPanel } from "../../components/Primitives";
@@ -7,6 +8,8 @@ import { withAuth } from "../../components/hoc/withAuth";
 import { useUser } from "../../components/hooks/useUser";
 
 const Profile = ({ seed }) => {
+  const { addToast } = useToasts();
+
   const handleSubmit = (values, { setSubmitting }) =>
     axios
       .put(`/api/me`, values)
@@ -24,11 +27,10 @@ const Profile = ({ seed }) => {
       <Panel
         header={
           <div className="space-y-1">
-            <h1 className="text-lg leading-6 font-medium text-gray-900 dark:text-white">
-              User Profile
-            </h1>
+            <h1 className="text-lg leading-6 font-medium text-gray-900 dark:text-white">User Profile</h1>
           </div>
-        }>
+        }
+      >
         <Formik enableReinitialize initialValues={user} onSubmit={handleSubmit}>
           {({ isSubmitting }) => (
             <Form>
@@ -37,12 +39,7 @@ const Profile = ({ seed }) => {
                   <TextField label="Firstname" name="firstname" type="text" autocomplete="none" />
                   <TextField label="Lastname" name="lastname" type="text" autocomplete="none" />
                   <TextField label="Email" name="email" type="email" autocomplete="none" />
-                  <TextField
-                    label="New Password"
-                    name="password"
-                    type="password"
-                    autocomplete="none"
-                  />
+                  <TextField label="New Password" name="password" type="password" autocomplete="none" />
                 </div>
 
                 <div className="flex items-center justify-end space-x-4">
