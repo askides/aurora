2aaf522e0346fe7996d3edef8cad74a85b2d288a
Renato Pozzi <r.pozzi@bluecube.it>
2021-04-21 00:40:54 +0200

    Moved hooks under proper directory

--- pages/websites/create.js
+++ pages/websites/create.js
@@ -1,53 +1,78 @@
 import axios from "axios";
 import { Formik, Form } from "formik";
 import { useRouter } from "next/router";
-
-import { Button, TextField } from "../../components/AuroraForm";
-import { Panel } from "../../components/Primitives";
-import { withAuth } from "../../components/hoc/withAuth";
+import { PageHeading } from "../../components/PageHeading";
+import { TextField } from "../../components/TextField";
+import { Radio } from "../../components/Radio";
+import { withAuth } from "../../hoc/withAuth";
 
 const Create = () => {
   const router = useRouter();
-  const initialValues = { name: "", url: "" };
+  const breadcumbs = ["Websites", "Create"];
+  const initialValues = { name: "", url: "", share: "no" };
 
   const handleSubmit = (values, { setSubmitting }) =>
     axios
       .post("/api/me/websites", values)
       .then((res) => res.data.data)
-      .then((res) => router.push(`/websites/${res.seed}/edit`))
-      .catch((err) => console.log(err))
-      .finally(() => setSubmitting(false));
+      .then((res) => router.push(`/websites/${res.seed}/edit`));
 
   return (
-    <div className="h-full rounded-lg space-y-4 bg-gray-900">
-      <Panel
-        header={
-          <div className="space-y-1">
-            <h1 className="text-lg leading-6 font-medium text-gray-900 dark:text-white">
-              Create Website
-            </h1>
-            <p className="text-sm leading-5 text-gray-500 dark:text-white">
-              Insert the Website information by filling in the form below.
-            </p>
-          </div>
-        }>
-        <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-          {({ isSubmitting }) => (
+    <div className="flex justify-center">
+      <div>
+        <PageHeading title={"Create Website"} breadcumbs={breadcumbs} />
+
+        <div className="mt-8">
+          <Formik initialValues={initialValues} onSubmit={handleSubmit}>
             <Form>
-              <div className="space-y-6">
-                <div className="space-y-4">
-                  <TextField label="Website Name" name="name" type="text" autocomplete="none" />
-                  <TextField label="Website URL" name="url" type="text" autocomplete="none" />
+              <div className="space-y-8 divide-y divide-gray-200">
+                <div className="space-y-8 divide-y divide-gray-200">
+                  <div>
+                    <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
+                      <div className="sm:col-span-6">
+                        <TextField label="Website Name" name="name" type="text" autocomplete="none" />
+                      </div>
+
+                      <div className="sm:col-span-6">
+                        <TextField label="Website URL" name="url" type="text" autocomplete="none" />
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="pt-8">
+                    <div>
+                      <h3 className="text-lg leading-6 font-medium text-gray-900">Share Statistics</h3>
+                      <p className="mt-1 text-sm text-gray-500">
+                        If you select to share statistics, a public URL will be available presenting a read-only version
+                        of the Aurora Dashboard. You can disable it later.
+                      </p>
+                    </div>
+                    <div className="mt-6">
+                      <fieldset>
+                        <div className="space-y-4">
+                          <Radio value={"yes"} label="Yes, make it public." name="share" />
+                          <Radio value={"no"} label="Nope, i wanna keep it private." name="share" />
+                        </div>
+                      </fieldset>
+                    </div>
+                  </div>
                 </div>
 
-                <div className="flex items-center justify-end space-x-4">
-                  <Button type="submit" isLoading={isSubmitting} label="Create" />
+                <div className="pt-5">
+                  <div className="flex justify-end">
+                    <button
+                      type="submit"
+                      className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                      Save
+                    </button>
+                  </div>
                 </div>
               </div>
             </Form>
-          )}
-        </Formik>
-      </Panel>
+          </Formik>
+        </div>
+      </div>
     </div>
   );
 };
