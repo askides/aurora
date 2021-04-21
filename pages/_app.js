ed73aaf7d74a6c66dd206048c9e803ab0986f305
Renato Pozzi <r.pozzi@bluecube.it>
2021-04-21 22:54:42 +0200

    refactor app
    moved away AuthLayout

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,17 @@
 import { useRouter } from "next/router";
 import { ToastProvider } from "react-toast-notifications";
 import { Main } from "../components/layouts/Main";
-import MainLayout from "../components/layouts/MainLayout";
-import AuthLayout from "../components/layouts/AuthLayout";
 
 import "tailwindcss/tailwind.css";
 import "../assets/css/scrollbar.css";
 
+const isAuthPath = (path) => path.startsWith("/auth");
+
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
-  if (router.pathname.startsWith("/auth")) {
-    return (
-      <AuthLayout>
-        <Component {...pageProps} />
-      </AuthLayout>
-    );
+  if (isAuthPath(router.pathname)) {
+    return <Component {...pageProps} />;
   }
 
   return (
