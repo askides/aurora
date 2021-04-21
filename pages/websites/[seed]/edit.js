386c4ff531b40ee6f5a8c132af95bb01dcad777b
Renato Pozzi <r.pozzi@bluecube.it>
2021-04-21 22:55:01 +0200

    refactor with Button component

--- pages/websites/[seed]/edit.js
+++ pages/websites/[seed]/edit.js
@@ -3,6 +3,7 @@ import { Formik, Form } from "formik";
 import { PageHeading } from "../../../components/PageHeading";
 import { TextField } from "../../../components/TextField";
 import { Radio } from "../../../components/Radio";
+import { Button } from "../../../components/Button";
 import { withAuth } from "../../../hoc/withAuth";
 import { useMeWebsite } from "../../../hooks/useMeWebsite";
 
@@ -36,53 +37,50 @@ const Edit = ({ seed }) => {
 
         <div className="mt-8">
           <Formik initialValues={website} onSubmit={handleSubmit}>
-            <Form>
-              <div className="space-y-8 divide-y divide-gray-200">
+            {({ isSubmitting }) => (
+              <Form>
                 <div className="space-y-8 divide-y divide-gray-200">
-                  <div>
-                    <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
-                      <div className="sm:col-span-6">
-                        <TextField label="Website Name" name="name" type="text" autocomplete="none" />
-                      </div>
-
-                      <div className="sm:col-span-6">
-                        <TextField label="Website URL" name="url" type="text" autocomplete="none" />
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="pt-8">
+                  <div className="space-y-8 divide-y divide-gray-200">
                     <div>
-                      <h3 className="text-lg leading-6 font-medium text-gray-900">Share Statistics</h3>
-                      <p className="mt-1 text-sm text-gray-500">
-                        If you select to share statistics, a public URL will be available presenting a read-only version
-                        of the Aurora Dashboard. You can disable it later.
-                      </p>
-                    </div>
-
-                    <div className="mt-6">
-                      <fieldset>
-                        <div className="space-y-4">
-                          <Radio value={"yes"} label="Yes, make it public." name="share" />
-                          <Radio value={"no"} label="Nope, i wanna keep it private." name="share" />
+                      <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
+                        <div className="sm:col-span-6">
+                          <TextField label="Website Name" name="name" type="text" autocomplete="none" />
                         </div>
-                      </fieldset>
+
+                        <div className="sm:col-span-6">
+                          <TextField label="Website URL" name="url" type="text" autocomplete="none" />
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="pt-8">
+                      <div>
+                        <h3 className="text-lg leading-6 font-medium text-gray-900">Share Statistics</h3>
+                        <p className="mt-1 text-sm text-gray-500">
+                          If you select to share statistics, a public URL will be available presenting a read-only
+                          version of the Aurora Dashboard. You can disable it later.
+                        </p>
+                      </div>
+
+                      <div className="mt-6">
+                        <fieldset>
+                          <div className="space-y-4">
+                            <Radio value={"yes"} label="Yes, make it public." name="share" />
+                            <Radio value={"no"} label="Nope, i wanna keep it private." name="share" />
+                          </div>
+                        </fieldset>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="pt-5">
+                    <div className="flex justify-end">
+                      <Button type="submit" value="Save" isLoading={isSubmitting} />
                     </div>
                   </div>
                 </div>
-
-                <div className="pt-5">
-                  <div className="flex justify-end">
-                    <button
-                      type="submit"
-                      className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                      Save
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </Form>
+              </Form>
+            )}
           </Formik>
         </div>
       </div>
