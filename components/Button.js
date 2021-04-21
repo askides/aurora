65563da665157dc708381ec2e69f9dc205732ed2
Renato Pozzi <r.pozzi@bluecube.it>
2021-04-21 22:47:49 +0200

    Enhanced button with tests

--- components/Button.js
+++ components/Button.js
@@ -1,13 +1,20 @@
-export const Button = ({ value, onClick }) => (
+import { Loader } from "../components/Icons";
+
+export const Button = ({ type, value, isLoading, onClick }) => (
   <button
-    type="button"
+    type={type}
+    disabled={isLoading}
     onClick={onClick}
     className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
   >
+    {isLoading && <Loader />}
     {value}
   </button>
 );
 
 Button.defaultProps = {
+  type: "button",
+  value: "",
+  isLoading: false,
   onClick: () => {},
 };
