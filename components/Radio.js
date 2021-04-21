7dc7e7dc66073b0eec4cc75dfd30d164211a2b71
Renato Pozzi <r.pozzi@bluecube.it>
2021-04-22 00:04:53 +0200

    fixed radio component

--- components/Radio.js
+++ components/Radio.js
@@ -3,10 +3,13 @@ import { useField } from "formik";
 export const Radio = ({ label, value, ...props }) => {
   const [field, meta] = useField(props);
 
+  const isSelected = (v) => v == meta.value;
+
   return (
     <div className="flex items-center">
       <input
         {...field}
+        checked={isSelected(value)}
         value={value}
         type="radio"
         className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
