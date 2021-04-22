79c7097805faecd4a495e2c7ace80732f15ab497
Renato Pozzi <r.pozzi@bluecube.it>
2021-04-22 22:07:52 +0200

    Removed no more used components

--- components/PageHeading.js
+++ components/PageHeading.js
@@ -1,4 +1,3 @@
-/* This example requires Tailwind CSS v2.0+ */
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/solid";
 
 export const PageHeading = ({ title, breadcumbs, actions }) => {
