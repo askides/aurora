4853c1df905d60e0b1260b24b2a46066727c4ed9
Renato Pozzi <r.pozzi@bluecube.it>
2021-04-25 14:15:55 +0200

    added hidden fields to website

--- models/Website.js
+++ models/Website.js
@@ -3,6 +3,7 @@ import { User } from "./User";
 
 export const Website = bookshelf.model("Website", {
   tableName: "websites",
+  hidden: ["id", "user_id"],
 
   user() {
     return this.belongsTo(User);
