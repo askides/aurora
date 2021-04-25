760bffec0215a15c6bad34cfcbb4904d45ed74e8
Renato Pozzi <r.pozzi@bluecube.it>
2021-04-25 20:31:34 +0200

    fixed sum & updated tests

--- utils/__tests__/hash.test.js
+++ utils/__tests__/hash.test.js
@@ -1,17 +1,17 @@
-const { hash, verify } = require("../../../utils/hash");
+const { hash, verify } = require("../hash");
 
 describe("Testing Hash & Verify Works!", () => {
   it("testing correct hash", () => {
     const plainTextWord = "password";
     const hashedWord = hash(plainTextWord);
 
-    expect(verify(plainTextWord, hashedWord)).to.eq(true);
+    expect(verify(plainTextWord, hashedWord)).toBe(true);
   });
 
   it("testing incorrect hash", () => {
     const plainTextWord = "password";
     const hashedWord = hash("passwordDifferent");
 
-    expect(verify(plainTextWord, hashedWord)).to.eq(false);
+    expect(verify(plainTextWord, hashedWord)).toBe(false);
   });
 });
--- utils/math.js
+++ utils/math.js
@@ -1,5 +1,5 @@
 const percentage = (n, t) => (n / t) * 100;
 
-const sum = (args) => args.reduce((acc, el) => acc + el);
+const sum = (args = []) => args.reduce((acc, el) => acc + el, 0);
 
 module.exports = { percentage, sum };
