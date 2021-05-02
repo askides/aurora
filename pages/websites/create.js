6ce496aae4def24a994a224c451894e682e47b00
Saurish <saurishs1215@gmail.com>
2021-05-02 10:54:26 -0700

    [fix typos]

--- pages/websites/create.js
+++ pages/websites/create.js
@@ -20,7 +20,7 @@ const Create = () => {
       .finally(setSubmitting(false));
 
   return (
-    <div className="p-6 h-full">
+    <div className="h-full p-6">
       <div className="flex justify-center">
         <div>
           <PageHeading title={"Create Website"} breadcumbs={breadcumbs} />
@@ -32,30 +32,47 @@ const Create = () => {
                   <div className="space-y-8 divide-y divide-gray-200">
                     <div className="space-y-8 divide-y divide-gray-200">
                       <div>
-                        <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
+                        <div className="grid grid-cols-1 mt-6 gap-y-6 gap-x-4 sm:grid-cols-6">
                           <div className="sm:col-span-6">
-                            <TextField label="Website Name" name="name" type="text" autocomplete="none" />
+                            <TextField
+                              label="Website Name"
+                              name="name"
+                              type="text"
+                              autocomplete="none"
+                            />
                           </div>
 
                           <div className="sm:col-span-6">
-                            <TextField label="Website URL" name="url" type="text" autocomplete="none" />
+                            <TextField
+                              label="Website URL"
+                              name="url"
+                              type="text"
+                              autocomplete="none"
+                            />
                           </div>
                         </div>
                       </div>
 
                       <div className="pt-8">
                         <div>
-                          <h3 className="text-lg leading-6 font-medium text-gray-900">Share Statistics</h3>
+                          <h3 className="text-lg font-medium leading-6 text-gray-900">
+                            Share Statistics
+                          </h3>
                           <p className="mt-1 text-sm text-gray-500">
-                            If you select to share statistics, a public URL will be available presenting a read-only
-                            version of the Aurora Dashboard. You can disable it later.
+                            If you choose to make statistics public, a public URL will be available
+                            presenting a read-only version of the Aurora Dashboard. Don't worry, you
+                            can disable it later!
                           </p>
                         </div>
                         <div className="mt-6">
                           <fieldset>
                             <div className="space-y-4">
                               <Radio value="1" label="Yes, make it public." name="shared" />
-                              <Radio value="0" label="Nope, i wanna keep it private." name="shared" />
+                              <Radio
+                                value="0"
+                                label="Nope, I want to keep it private."
+                                name="shared"
+                              />
                             </div>
                           </fieldset>
                         </div>
