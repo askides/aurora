91466f233a49f731bba4df39226befdb0f5c6fa5
Renato Pozzi <r.pozzi@bluecube.it>
2021-05-10 17:47:14 +0200

    new area chart

--- components/charts/Area.js
+++ components/charts/Area.js
@@ -1,112 +1,106 @@
 import Chart from "react-apexcharts";
+import { useGraph } from "../../hooks/useGraph";
 
-const Area = () => {
-  const data = {
-    series: [
-      {
-        name: "series1",
-        data: [31, 40, 28, 100, 51, 300, 420, 109, 100, 1230],
-      },
-    ],
-    options: {
-      chart: {
-        height: 350,
-        type: "area",
-        toolbar: {
-          show: false,
-        },
-        zoom: {
-          enabled: false,
-        },
-        // sparkline: {
-        //   enabled: true,
-        // },
-      },
-      grid: {
-        row: {
-          // colors: ["#e5e5e5", "transparent"],
-          opacity: 0.5,
-        },
-        column: {
-          //colors: ["#f8f8f8", "transparent"],
-        },
-        yaxis: {
-          lines: {
-            show: false,
-          },
-        },
-        xaxis: {
-          lines: {
-            show: false,
-          },
-        },
+const format = ({ labels }) => ({
+  chart: {
+    height: 350,
+    type: "area",
+    toolbar: {
+      show: false,
+    },
+    zoom: {
+      enabled: false,
+    },
+    sparkline: {
+      enabled: true,
+    },
+  },
+  grid: {
+    row: {
+      opacity: 0.5,
+    },
+    yaxis: {
+      lines: {
         show: false,
-        padding: {
-          left: 0,
-          right: 0,
-        },
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      stroke: {
-        curve: "smooth",
-      },
-      yaxis: {
-        labels: {
-          show: true,
-          style: {
-            colors: "#FFFFFF",
-            fontSize: "14px",
-            fontWeight: 800,
-            cssClass: "apexcharts-xaxis-label",
-          },
-          padding: {
-            left: 0,
-            right: 0,
-          },
-        },
-      },
-      xaxis: {
-        type: "datetime",
-        categories: [
-          "2018-09-19T00:00:00.000Z",
-          "2018-09-19T01:30:00.000Z",
-          "2018-09-19T02:30:00.000Z",
-          "2018-09-19T03:30:00.000Z",
-          "2018-09-19T04:30:00.000Z",
-          "2018-09-19T05:30:00.000Z",
-          "2018-09-19T06:30:00.000Z",
-          "2018-09-19T07:30:00.000Z",
-          "2018-09-19T08:30:00.000Z",
-          "2018-09-19T09:30:00.000Z",
-          "2018-09-19T010:30:00.000Z",
-        ],
-        axisBorder: {
-          show: false,
-        },
-        lines: {
-          show: false,
-        },
-        labels: {
-          show: true,
-          style: {
-            colors: "#FFFFFF",
-            fontSize: "14px",
-            fontWeight: 800,
-            cssClass: "apexcharts-xaxis-label",
-          },
-        },
-      },
-      tooltip: {
-        x: {
-          format: "dd/MM/yy HH:mm",
-        },
       },
     },
-  };
+    xaxis: {
+      lines: {
+        show: false,
+      },
+    },
+    show: false,
+    padding: {
+      left: 0,
+      right: 0,
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    curve: "smooth",
+  },
+  yaxis: {
+    labels: {
+      show: true,
+      style: {
+        colors: "#FFFFFF",
+        fontSize: "14px",
+        fontWeight: 800,
+        cssClass: "apexcharts-xaxis-label",
+      },
+      padding: {
+        left: 0,
+        right: 0,
+      },
+    },
+  },
+  xaxis: {
+    type: "datetime",
+    categories: labels,
+    axisBorder: {
+      show: false,
+    },
+    lines: {
+      show: false,
+    },
+    labels: {
+      show: true,
+      style: {
+        colors: "#FFFFFF",
+        fontSize: "14px",
+        fontWeight: 800,
+        cssClass: "apexcharts-xaxis-label",
+      },
+    },
+    tooltip: {
+      enabled: false,
+    },
+  },
+  tooltip: {
+    x: {
+      format: "dd/MM/yy HH:mm",
+    },
+  },
+});
 
-  return <Chart options={data.options} series={data.series} type="area" height={350} />;
+const Area = ({ url, timeRange }) => {
+  const { graph, isLoading, isError } = useGraph(url, timeRange);
+
+  if (isLoading) return <div>Loading ...</div>;
+  if (isError) return <div>failed to load</div>;
+
+  console.log("grapg", graph);
+
+  return (
+    <Chart
+      options={format({ labels: graph.labels })}
+      series={graph.series}
+      type="area"
+      height={350}
+    />
+  );
 };
 
 export default Area;
