47492376fb6da8db279c66d3c7163e32818376a4
Renato Pozzi <r.pozzi@bluecube.it>
2021-05-10 23:06:18 +0200

    added test env

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,25 @@
 require("dotenv").config();
 
 module.exports = {
+  test: {
+    client: "pg",
+    connection: {
+      connectionString: process.env.DB_URL,
+      ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
+      // ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+    pool: {
+      min: 0,
+      max: 10,
+      acquireTimeoutMillis: 30000,
+      createTimeoutMillis: 1500,
+      createRetryIntervalMillis: 500,
+      propagateCreateError: false,
+    },
+  },
   development: {
     client: "pg",
     connection: {
