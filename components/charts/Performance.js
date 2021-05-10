ea82cec86082d7eb3a278bde0d898fbfde6dbcc4
Renato Pozzi <r.pozzi@bluecube.it>
2021-05-10 17:46:34 +0200

    new Performance Component

--- components/charts/Performance.js
+++ components/charts/Performance.js
@@ -1,6 +1,5 @@
-import { Stats } from "../Stats";
-import { useGraph } from "../../hooks/useGraph";
 import { Jumbo } from "./Jumbo";
+import { useGraph } from "../../hooks/useGraph";
 
 export const Performance = ({ url, timeRange }) => {
   const { graph, isLoading, isError } = useGraph(url, timeRange);
@@ -9,39 +8,11 @@ export const Performance = ({ url, timeRange }) => {
   if (isError) return <div>failed to load</div>;
 
   return (
-    <dl className="grid grid-cols-1 gap-5 sm:grid-cols-4 divide-x divide-gray-800">
+    <dl className="grid grid-cols-1 gap-5 sm:grid-cols-4 sm:divide-x divide-gray-800">
       <Jumbo title="Total Views" value={graph.pageViews.cp} />
       <Jumbo title="Unique Visitors" value={graph.uniqueVisitors.cp} />
       <Jumbo title="Bounces" value={graph.bounceRate.cp} />
       <Jumbo title="Avg Visit Time" value="24s" />
     </dl>
-
-    /*
-    <Stats
-      stats={[
-        {
-          name: "Total Views",
-          stat: graph.pageViews.cp,
-          previousStat: graph.pageViews.lp,
-          change: graph.pageViews.inc,
-          changeType: graph.pageViews.inc >= 0 ? "increase" : "decrease",
-        },
-        {
-          name: "Unique Visitors",
-          stat: graph.uniqueVisitors.cp,
-          previousStat: graph.uniqueVisitors.lp,
-          change: graph.uniqueVisitors.inc,
-          changeType: graph.uniqueVisitors.inc >= 0 ? "increase" : "decrease",
-        },
-        {
-          name: "Bounces",
-          stat: graph.bounceRate.cp,
-          previousStat: graph.bounceRate.lp,
-          change: graph.bounceRate.inc,
-          changeType: graph.bounceRate.inc >= 0 ? "increase" : "decrease",
-        },
-      ]}
-    />
-    */
   );
 };
