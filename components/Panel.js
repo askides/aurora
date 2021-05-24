a47bc1f149337e00f4b862a43babb737f10db642
Renato Pozzi <r.pozzi@bluecube.it>
2021-05-24 19:42:22 +0200

    fix bg

--- components/Panel.js
+++ components/Panel.js
@@ -1,6 +1,6 @@
 export const Panel = ({ header, children }) => {
   return (
-    <div className="bg-white dark:bg-gray-900 overflow-hidden sm:first:pl-0 sm:pl-5 sm:py-5">
+    <div className="bg-gray-900 overflow-hidden sm:first:pl-0 sm:pl-5 sm:py-5">
       {header && <div className="px-4 py-5 sm:px-6">{header}</div>}
       <div className="">{children}</div>
     </div>
