dca5d2cce1109066737403b5db704d0d00df91e2
Renato Pozzi <r.pozzi@bluecube.it>
2021-06-20 16:07:31 +0200

    removed no more used components

--- models/Browser.js
+++
@@ -1,8 +0,0 @@
-import { bookshelf } from "../lib/db_connect";
-import { Event } from "./Event";
-
-export const Browser = bookshelf.model("Browser", {
-  tableName: "browsers",
-
-  events: () => this.hasMany(Event),
-});
--- models/Engine.js
+++
@@ -1,8 +0,0 @@
-import { bookshelf } from "../lib/db_connect";
-import { Event } from "./Event";
-
-export const Engine = bookshelf.model("Engine", {
-  tableName: "engines",
-
-  events: () => this.hasMany(Event),
-});
