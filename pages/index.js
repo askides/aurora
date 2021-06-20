fbed5f21c28c24fe0cdb15c337869b28bff516b7
Renato Pozzi <r.pozzi@bluecube.it>
2021-06-20 17:14:42 +0200

    refactoring index for responsive

--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,12 @@
 import Head from "next/head";
 import { default as NextLink } from "next/link";
-import { HeadlessTable } from "../components/HeadlessTable";
 import { PageHeading } from "../components/PageHeading";
 import { Link } from "../components/Link";
 import { useMeWebsites } from "../hooks/useMeWebsites";
 import { withAuth } from "../hoc/withAuth";
 
-const columns = [
-  {
-    Header: "Name",
-    accessor: "name",
-    Cell: ({ cell, value }) => (
-      <div className="flex items-center justify-between space-x-3">
-        <div className="flex items-center truncate space-x-3">
-          <div className="flex-shrink-0 w-2.5 h-2.5 rounded-full bg-pink-600"></div>
-          <NextLink href={`/websites/${cell.row.original.seed}/edit`}>
-            <a>{value}</a>
-          </NextLink>
-        </div>
-
-        <svg
-          className="block sm:hidden ml-4 h-5 w-5 text-gray-400 group-hover:text-gray-500 group-focus:text-gray-600 transition ease-in-out duration-150"
-          xmlns="https://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fillRule="evenodd"
-            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-            clipRule="evenodd"
-          ></path>
-        </svg>
-      </div>
-    ),
-  },
-  { Header: "Url", accessor: "url" },
-  { Header: "Shared", accessor: (row) => (row.shared ? "Yes" : "No") },
-  {
-    id: "actions",
-    Cell: ({ cell }) => (
-      <div className="flex items-center justify-end space-x-3 font-medium text-blue-600 text-right">
-        <NextLink href={`/websites/${cell.row.original.seed}`}>
-          <a>View Dashboard</a>
-        </NextLink>
-      </div>
-    ),
-  },
-];
-
 const Websites = () => {
   const { websites, isLoading, isError } = useMeWebsites();
-
-  const breadcumbs = ["Websites", "List"];
 
   if (isLoading) return <div>Loading..</div>;
   if (isError) return <div>failed to load</div>;
@@ -64,12 +19,46 @@ const Websites = () => {
 
       <PageHeading
         title={"Websites"}
-        breadcumbs={breadcumbs}
+        breadcumbs={["Websites", "List"]}
         actions={<Link value="Create New" href="/websites/create" />}
       />
 
       <div className="mt-8">
-        <HeadlessTable data={websites} columns={columns} onFetchData={() => {}} />
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {websites.map((website) => (
+            <div className="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200">
+              <div className="w-full flex items-center justify-between p-6 space-x-6">
+                <div className="flex-1 truncate">
+                  <div className="flex items-center space-x-3">
+                    <h3 className="text-gray-900 text-sm font-medium truncate">{website.name}</h3>
+                  </div>
+                  <p className="mt-1 text-gray-500 text-sm truncate">{website.url}</p>
+                </div>
+                <span className="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-sm font-medium bg-green-100 rounded-full">
+                  {website.shared ? "Public" : "Private"}
+                </span>
+              </div>
+              <div>
+                <div className="-mt-px flex divide-x divide-gray-200">
+                  <div className="w-0 flex-1 flex">
+                    <NextLink href={`/websites/${website.seed}/edit`}>
+                      <a className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500">
+                        <span className="ml-3">Edit</span>
+                      </a>
+                    </NextLink>
+                  </div>
+                  <div className="-ml-px w-0 flex-1 flex">
+                    <NextLink href={`/websites/${website.seed}`}>
+                      <a className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500">
+                        <span className="ml-3">Dashboard</span>
+                      </a>
+                    </NextLink>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
