4ca7dc3aa48a55b38c574f14ef354a0ed097af3c
Renato Pozzi <r.pozzi@bluecube.it>
2021-06-22 22:52:09 +0200

    refactor

--- pages/websites/[seed]/index.js
+++ pages/websites/[seed]/index.js
@@ -1,14 +1,6 @@
-import dynamic from "next/dynamic";
-import { useState } from "react";
 import { withAuth } from "../../../hoc/withAuth";
-import { useWebsite } from "../../../hooks/useWebsite";
-import { TimeRanges } from "../../../utils/enums";
-import { dropProtocol } from "../../../utils/urls";
-import { Performance } from "../../../components/charts/Performance";
-import { RealtimeVisitors } from "../../../components/RealtimeVisitors";
-import { RangeSelector } from "../../../components/RangeSelector";
-import { Linear } from "../../../components/charts/Linear";
 import { Container } from "../../../components/Container";
+import { Dashboard } from "../../../components/Dashboard";
 
 export async function getServerSideProps(context) {
   const { seed } = context.query;
@@ -18,15 +10,10 @@ export async function getServerSideProps(context) {
   };
 }
 
-const Area = dynamic(() => import("../../../components/charts/Area"), { ssr: false });
-
 const Website = ({ seed }) => {
-  const { website, isLoading, isError } = useWebsite({ seed });
-  const [timeRange, setTimeRange] = useState(TimeRanges.DAY);
-
   return (
     <Container>
-      <div className="flex flex-col justify-center items-start max-w-5xl w-full mx-auto mb-16">
+      <div className="flex flex-col justify-center items-start max-w-6xl w-full mx-auto mb-16">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
           Dashboard
         </h1>
@@ -35,50 +22,7 @@ const Website = ({ seed }) => {
           These are your websites, you can manage them by clicking on the proper buttons.
         </p>
 
-        <div className="border border-gray-200 dark:border-gray-800 rounded-lg p-4 sm:p-8 mt-8 w-full space-y-10">
-          <div className="md:flex md:items-center md:justify-between">
-            <div className="flex-1 min-w-0">
-              <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate text-black dark:text-white">
-                {isLoading ? "_" : dropProtocol(website.url)}
-              </h2>
-
-              <div className="pt-2 flex flex-col sm:flex-row sm:flex-wrap sm:mt-0 sm:space-x-6">
-                <RealtimeVisitors seed={seed} />
-              </div>
-            </div>
-
-            <div className="mt-4 flex-shrink-0 flex md:mt-0 md:ml-4 space-x-3">
-              <RangeSelector onSelected={(value) => setTimeRange(value)} />
-            </div>
-          </div>
-
-          <Performance url={`/v2/metrics/${seed}/performance`} timeRange={timeRange} />
-
-          <Area url={`/v2/metrics/${seed}/views/series`} timeRange={timeRange} />
-
-          <div className="grid md:grid-cols-3 gap-4 gap-y-10 sm:divide-x divide-gray-200 dark:divide-gray-800">
-            <Linear title="Os" url={`/v2/metrics/${seed}/views/os`} timeRange={timeRange} />
-            <Linear
-              title="Browser"
-              url={`/v2/metrics/${seed}/views/browser`}
-              timeRange={timeRange}
-            />
-            <Linear
-              title="Country"
-              url={`/v2/metrics/${seed}/views/country`}
-              timeRange={timeRange}
-            />
-          </div>
-
-          <div className="grid md:grid-cols-2 gap-4 pt-6 sm:pt-0 gap-y-10 sm:divide-x divide-gray-200 dark:divide-gray-800">
-            <Linear title="Page" url={`/v2/metrics/${seed}/views/page`} timeRange={timeRange} />
-            <Linear
-              title="Referrer"
-              url={`/v2/metrics/${seed}/views/referrer`}
-              timeRange={timeRange}
-            />
-          </div>
-        </div>
+        <Dashboard seed={seed} />
       </div>
     </Container>
   );
