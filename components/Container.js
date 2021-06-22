814afb3796d442d4830cc83efa09e4bfa451cd42
Renato Pozzi <r.pozzi@bluecube.it>
2021-06-22 22:51:54 +0200

    added navbar option

--- components/Container.js
+++ components/Container.js
@@ -1,16 +1,14 @@
 import Head from "next/head";
 import { Navbar } from "./Navbar";
 
-export const Container = (props) => {
-  const { children } = props;
-
+export const Container = ({ children, navbar = true }) => {
   return (
     <div className="bg-white dark:bg-black">
       <Head>
         <title>Aurora - Open Website Analytics</title>
       </Head>
 
-      <Navbar />
+      {navbar && <Navbar />}
 
       <main className="flex flex-col justify-center px-8 bg-white dark:bg-black">{children}</main>
     </div>
