45583798a8d8dd365914cd2de5abc5171c8c8d61
Renato Pozzi <r.pozzi@bluecube.it>
2021-06-27 23:26:02 +0200

    added default dark

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import { ThemeProvider } from "next-themes";
 
 const App = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider attribute="class">
+    <ThemeProvider attribute="class" defaultTheme="dark">
       <Component {...pageProps} />
     </ThemeProvider>
   );
