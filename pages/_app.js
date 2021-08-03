294a1adc3bab9e618bf5a931b430a58a4bbc5661
Renato Pozzi <r.pozzi@bluecube.it>
2021-08-04 00:17:10 +0200

    added toaster provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import "../styles/globals.css";
 
+import { Toaster } from "react-hot-toast";
 import { ThemeProvider } from "next-themes";
 
 const App = ({ Component, pageProps }) => {
   return (
     <ThemeProvider attribute="class" defaultTheme="dark">
       <Component {...pageProps} />
+      <Toaster position="top-right" />
     </ThemeProvider>
   );
 };
