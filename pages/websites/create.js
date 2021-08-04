3756a5a333311c9d8e1ffecf0a31307c8701a874
Renato Pozzi <r.pozzi@bluecube.it>
2021-08-04 23:49:41 +0200

    expoted input

--- pages/websites/create.js
+++ pages/websites/create.js
@@ -2,26 +2,10 @@ import toast from "react-hot-toast";
 import { useRouter } from "next/router";
 import { useForm } from "react-hook-form";
 import { Button } from "../../components/Button";
-import { client } from "../../utils/api";
+import { Input } from "../../components/Input";
 import { Container } from "../../components/Container";
 import { Page } from "../../components/Page";
-
-const Input = ({ register, name, type = "text", ...rest }) => (
-  <div>
-    {rest.label && (
-      <label className="block text-sm font-medium text-gray-600 dark:text-gray-100 mb-1">
-        {rest.label}
-      </label>
-    )}
-
-    <input
-      {...register(name)}
-      {...rest}
-      type={type}
-      className="text-black dark:text-gray-100 focus:ring-blue-500 focus:border-blue-500 dark:focus:border-blue-500 block w-full sm:text-sm border-gray-300 dark:border-gray-800 rounded-md bg-white dark:bg-black dark:placeholder-gray-500"
-    />
-  </div>
-);
+import { client } from "../../utils/api";
 
 const Radio = ({ register, name, ...rest }) => (
   <div className="flex items-center">
