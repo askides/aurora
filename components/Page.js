275aac64ad3d9b22e35829ced3b5b2465d40d3df
Renato Pozzi <r.pozzi@bluecube.it>
2021-08-05 23:35:55 +0200

    global footer & title variant page

--- components/Page.js
+++ components/Page.js
@@ -1,11 +1,15 @@
+import { Footer } from "./Footer";
+
 export const Page = (props) => {
   const maxWidth = props.fluid ? "max-w-6xl" : "max-w-3xl";
 
   return (
     <div className={`flex flex-col justify-center items-start ${maxWidth} w-full mx-auto mb-16`}>
-      <h1 className="font-bold text-3xl md:text-5xl tracking-tight text-black dark:text-white">
-        {props.title}
-      </h1>
+      {props.title && (
+        <h1 className="font-bold text-3xl md:text-5xl tracking-tight text-black dark:text-white">
+          {props.title}
+        </h1>
+      )}
 
       {props.subtitle && (
         <p className="prose leading-relaxed text-gray-600 dark:text-gray-400 my-4">
@@ -14,6 +18,8 @@ export const Page = (props) => {
       )}
 
       <div className="mt-8 w-full">{props.children}</div>
+
+      <Footer />
     </div>
   );
 };
