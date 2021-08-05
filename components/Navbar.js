78be8690a979c651030c013d5cb81314729a5786
Renato Pozzi <r.pozzi@bluecube.it>
2021-08-05 23:45:48 +0200

    dark mode to navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,12 +2,12 @@ import NextLink from "next/link";
 import { ToggleTheme } from "./ToggleTheme";
 
 export const Navbar = () => (
-  <section className="w-full px-8 text-gray-700 bg-white">
+  <section className="w-full px-8 text-gray-700 bg-transparent">
     <div className="container flex flex-col flex-wrap items-center justify-between py-5 mx-auto md:flex-row max-w-4xl">
       <div className="relative flex flex-col md:flex-row">
         <NextLink href="/">
           <a className="flex items-center mb-5 font-medium text-gray-900 lg:w-auto lg:items-center lg:justify-center md:mb-0">
-            <span className="mx-auto text-xl font-black leading-none text-gray-900 select-none">
+            <span className="mx-auto text-xl font-black leading-none text-gray-900 dark:text-gray-100 select-none">
               useaurora
             </span>
           </a>
@@ -15,18 +15,22 @@ export const Navbar = () => (
 
         <nav className="flex flex-wrap items-center mb-5 text-base md:mb-0 md:pl-8 md:ml-8 md:border-l md:border-gray-200 space-x-5">
           <NextLink href="/">
-            <a className="font-medium leading-6 text-gray-600 hover:text-gray-900">Websites</a>
+            <a className="font-medium leading-6 text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-500">
+              Websites
+            </a>
           </NextLink>
 
           <NextLink href="/profile">
-            <a className="font-medium leading-6 text-gray-600 hover:text-gray-900">Profile</a>
+            <a className="font-medium leading-6 text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-500">
+              Profile
+            </a>
           </NextLink>
         </nav>
       </div>
 
       <div className="inline-flex items-center space-x-6 lg:justify-end">
         <NextLink href="/login">
-          <a className="text-base font-medium leading-6 text-gray-600 whitespace-no-wrap transition duration-150 ease-in-out hover:text-gray-900">
+          <a className="text-base font-medium leading-6 whitespace-no-wrap transition duration-150 ease-in-out text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-500">
             Sign in
           </a>
         </NextLink>
