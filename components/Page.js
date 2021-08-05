020992bfaf5a2b95d7c79f45dc564d0b274baba9
Renato Pozzi <r.pozzi@bluecube.it>
2021-08-05 23:07:49 +0200

    added fluid variant to page

--- components/Page.js
+++ components/Page.js
@@ -1,15 +1,19 @@
-export const Page = (props) => (
-  <div className="flex flex-col justify-center items-start max-w-3xl w-full mx-auto mb-16">
-    <h1 className="font-bold text-3xl md:text-5xl tracking-tight text-black dark:text-white">
-      {props.title}
-    </h1>
+export const Page = (props) => {
+  const maxWidth = props.fluid ? "max-w-6xl" : "max-w-3xl";
 
-    {props.subtitle && (
-      <p className="prose leading-relaxed text-gray-600 dark:text-gray-400 my-4">
-        {props.subtitle}
-      </p>
-    )}
+  return (
+    <div className={`flex flex-col justify-center items-start ${maxWidth} w-full mx-auto mb-16`}>
+      <h1 className="font-bold text-3xl md:text-5xl tracking-tight text-black dark:text-white">
+        {props.title}
+      </h1>
 
-    <div className="mt-8 w-full">{props.children}</div>
-  </div>
-);
+      {props.subtitle && (
+        <p className="prose leading-relaxed text-gray-600 dark:text-gray-400 my-4">
+          {props.subtitle}
+        </p>
+      )}
+
+      <div className="mt-8 w-full">{props.children}</div>
+    </div>
+  );
+};
