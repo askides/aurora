8c8f0d03fae2cdaccada2206e9f34ca9c3660bb3
Renato Pozzi <r.pozzi@bluecube.it>
2021-08-06 14:04:27 +0200

    changed padding

--- components/Container.js
+++ components/Container.js
@@ -12,9 +12,7 @@ export const Container = ({ children, navbar = true }) => {
         </div>
       )}
 
-      <main className="flex flex-col justify-center px-4 sm:px-8 bg-white dark:bg-black">
-        {children}
-      </main>
+      <main className="flex flex-col justify-center px-8 bg-white dark:bg-black">{children}</main>
     </div>
   );
 };
