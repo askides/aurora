0a558a57c680e7fbeb24f9e2da0dbd4eb2944b8f
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-03-04 12:11:11 +0100

    testing purposes

--- utils/dates.js
+++
@@ -1,3 +0,0 @@
-const localize = (date) => new Date(date).toLocaleDateString();
-
-module.exports = { localize };
--- utils/enums.js
+++
@@ -1,7 +0,0 @@
-/** TimeRanges Enumeration */
-export const TimeRanges = Object.freeze({
-  DAY: "day",
-  WEEK: "week",
-  MONTH: "month",
-  YEAR: "year",
-});
