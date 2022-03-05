1a500d6f9d5c41d44075c9df7b8def45e3920079
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-03-05 18:21:21 +0100

    refactor: index now supports new routing

--- packages/backend/__tests__/index.test.js
+++ packages/backend/__tests__/index.test.js
@@ -1,14 +1,13 @@
+import { createMocks } from "node-mocks-http";
 import handler from "../api/index";
-import { buildReq, buildRes } from "../utils/generate";
 
 it("should return 200 and a message", async () => {
-  const req = buildReq();
-  const res = buildRes();
+  const { req, res } = createMocks({ method: "GET" });
 
   await handler(req, res);
 
-  expect(res.status).toHaveBeenCalledWith(200);
-  expect(res.json.mock.calls[0][0]).toMatchInlineSnapshot(`
+  expect(res._getStatusCode()).toBe(200);
+  expect(res._getJSONData()).toMatchInlineSnapshot(`
     Object {
       "message": "Aurora APIs are running!",
     }
--- packages/backend/api/index.js
+++ packages/backend/api/index.js
@@ -1,3 +1,11 @@
-export default async function handler(_request, response) {
-  response.status(200).json({ message: "Aurora APIs are running!" });
+import { Router } from "../lib/router";
+
+export default async function handler(request, response) {
+  const router = new Router(request, response);
+
+  await router.route("GET", ({ res }) => {
+    return res.status(200).json({ message: "Aurora APIs are running!" });
+  });
+
+  router.fallback();
 }
