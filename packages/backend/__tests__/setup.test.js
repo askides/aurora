90d94eb255928d97141773d916f95e28da93926e
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-03-05 18:10:40 +0100

    feat: added new setup apis, based on the new simple routing system.

--- packages/backend/__tests__/setup.test.js
+++ packages/backend/__tests__/setup.test.js
@@ -1,6 +1,7 @@
+import { createMocks } from "node-mocks-http";
 import handler from "../api/setup";
 import * as AuroraDB from "../lib/database";
-import { buildReq, buildRes, buildUser } from "../utils/generate";
+import { buildUser } from "../utils/generate";
 
 beforeEach(async () => {
   await AuroraDB.client.user.deleteMany();
@@ -11,73 +12,80 @@ describe("non allowed methods should not work", () => {
 
   disallowedMethods.forEach((method) => {
     it(`should return 405 for ${method}`, async () => {
-      const req = buildReq({ method: method });
-      const res = buildRes();
+      const { req, res } = createMocks({ method });
 
       await handler(req, res);
 
-      expect(res.status).toHaveBeenCalledWith(405);
-      expect(res.json).toHaveBeenCalledWith({ message: "Method not allowed" });
+      expect(res._getStatusCode()).toBe(405);
+      expect(res._getJSONData()).toEqual({ message: "Method not allowed" });
     });
   });
 });
 
 it("should return that the setup is needed", async () => {
-  const req = buildReq({ method: "GET" });
-  const res = buildRes();
+  const { req, res } = createMocks({
+    method: "GET",
+  });
 
   await handler(req, res);
 
-  expect(res.status).toHaveBeenCalledWith(200);
-  expect(res.json).toHaveBeenCalledWith({ needsSetup: true });
+  expect(res._getStatusCode()).toBe(200);
+  expect(res._getJSONData()).toEqual({ needsSetup: true });
 });
 
 it("should return that the setup is not needed", async () => {
-  const req = buildReq({ method: "GET" });
-  const res = buildRes();
+  const { req, res } = createMocks({
+    method: "GET",
+  });
 
   await AuroraDB.createUser(buildUser());
   await handler(req, res);
 
-  expect(res.status).toHaveBeenCalledWith(200);
-  expect(res.json).toHaveBeenCalledWith({ needsSetup: false });
+  expect(res._getStatusCode()).toBe(400);
+  expect(res._getJSONData()).toMatchInlineSnapshot(`
+    Object {
+      "message": "The setup is already done",
+    }
+  `);
 });
 
 it("should create an user", async () => {
   const user = buildUser({}, { timestamps: false });
-  const req = buildReq({
+  const { req, res } = createMocks({
     method: "POST",
     body: {
       ...user,
       confirmPassword: user.password,
     },
   });
-  const res = buildRes();
 
   await handler(req, res);
 
-  expect(res.status).toHaveBeenCalledWith(201);
+  expect(res._getStatusCode()).toBe(201);
+  expect(res._getJSONData().data).toMatchObject({
+    ...user,
+    password: expect.any(String),
+  });
 
   // Ensure that the user is created
-  const createdUser = await AuroraDB.getUser(res.json.mock.calls[0][0].data.id);
+  const createdUser = await AuroraDB.getUser(res._getJSONData().data.id);
   expect(createdUser).toBeDefined();
 });
 
 it("should return an error when the password is not valid", async () => {
   const user = buildUser({ password: "12345" }, { timestamps: false });
-  const req = buildReq({
+  const { req, res } = createMocks({
     method: "POST",
     body: {
       ...user,
       confirmPassword: user.password,
     },
   });
-  const res = buildRes();
 
   await handler(req, res);
 
-  expect(res.status).toHaveBeenCalledWith(400);
-  expect(res.json.mock.calls[0][0]).toMatchInlineSnapshot(`
+  expect(res._getStatusCode()).toBe(400);
+  expect(res._getJSONData()).toMatchInlineSnapshot(`
     Object {
       "message": "\\"password\\" length must be at least 8 characters long",
     }
@@ -89,16 +97,16 @@ it("should return an error when the password is not confirmed", async () => {
     { password: "12345678", confirmPassword: "123456789" },
     { timestamps: false }
   );
-  const req = buildReq({
+
+  const { req, res } = createMocks({
     method: "POST",
     body: { ...user },
   });
-  const res = buildRes();
 
   await handler(req, res);
 
-  expect(res.status).toHaveBeenCalledWith(400);
-  expect(res.json.mock.calls[0][0]).toMatchInlineSnapshot(`
+  expect(res._getStatusCode()).toBe(400);
+  expect(res._getJSONData()).toMatchInlineSnapshot(`
     Object {
       "message": "\\"confirmPassword\\" must be [ref:password]",
     }
@@ -107,19 +115,19 @@ it("should return an error when the password is not confirmed", async () => {
 
 it("should return an error when the email is not valid", async () => {
   const user = buildUser({ email: "notanemail" }, { timestamps: false });
-  const req = buildReq({
+
+  const { req, res } = createMocks({
     method: "POST",
     body: {
       ...user,
       confirmPassword: user.password,
     },
   });
-  const res = buildRes();
 
   await handler(req, res);
 
-  expect(res.status).toHaveBeenCalledWith(400);
-  expect(res.json.mock.calls[0][0]).toMatchInlineSnapshot(`
+  expect(res._getStatusCode()).toBe(400);
+  expect(res._getJSONData()).toMatchInlineSnapshot(`
     Object {
       "message": "\\"email\\" must be a valid email",
     }
@@ -128,19 +136,19 @@ it("should return an error when the email is not valid", async () => {
 
 it("should return an error when firstname is not valid", async () => {
   const user = buildUser({ firstname: null }, { timestamps: false });
-  const req = buildReq({
+
+  const { req, res } = createMocks({
     method: "POST",
     body: {
       ...user,
       confirmPassword: user.password,
     },
   });
-  const res = buildRes();
 
   await handler(req, res);
 
-  expect(res.status).toHaveBeenCalledWith(400);
-  expect(res.json.mock.calls[0][0]).toMatchInlineSnapshot(`
+  expect(res._getStatusCode()).toBe(400);
+  expect(res._getJSONData()).toMatchInlineSnapshot(`
     Object {
       "message": "\\"firstname\\" must be a string",
     }
@@ -149,19 +157,19 @@ it("should return an error when firstname is not valid", async () => {
 
 it("should return an error when lastname is not valid", async () => {
   const user = buildUser({ lastname: null }, { timestamps: false });
-  const req = buildReq({
+
+  const { req, res } = createMocks({
     method: "POST",
     body: {
       ...user,
       confirmPassword: user.password,
     },
   });
-  const res = buildRes();
 
   await handler(req, res);
 
-  expect(res.status).toHaveBeenCalledWith(400);
-  expect(res.json.mock.calls[0][0]).toMatchInlineSnapshot(`
+  expect(res._getStatusCode()).toBe(400);
+  expect(res._getJSONData()).toMatchInlineSnapshot(`
     Object {
       "message": "\\"lastname\\" must be a string",
     }
---
+++ packages/backend/lib/controllers/setup-controller.js
@@ -0,0 +1,31 @@
+import Joi from "joi";
+import * as AuroraDB from "../database";
+
+export const SetupController = {
+  index: async ({ res }) => {
+    return res.status(200).json({ needsSetup: true });
+  },
+
+  store: async ({ req, res }) => {
+    const rules = Joi.object({
+      firstname: Joi.string().required(),
+      lastname: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().min(8).required(),
+      confirmPassword: Joi.string().valid(Joi.ref("password")).required(),
+    });
+
+    const {
+      error,
+      value: { confirmPassword, ...validated },
+    } = rules.validate(req.body);
+
+    if (error) {
+      return res.status(400).json({ message: error.message });
+    }
+
+    const createdUser = await AuroraDB.createUser(validated);
+
+    return res.status(201).json({ data: createdUser });
+  },
+};
