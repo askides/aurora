7a4f2b7712fd7722bfbc30ba1a979fd0ce0c533f
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-03-27 16:20:15 +0100

    feat: useForm hook & setup page

--- packages/frontend/src/pages/Setup.js
+++ packages/frontend/src/pages/Setup.js
@@ -1,46 +1,75 @@
 import {
   Button,
-  Center,
-  Container,
-  Loader,
-  TextInput,
-  Title,
-} from "@mantine/core";
+  Flex,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Heading,
+  Input,
+  VStack,
+} from "@chakra-ui/react";
 import * as React from "react";
+import { useForm } from "../lib/hooks/use-form";
 
-const useNeedsSetup = () => {
-  const [needsSetup, setNeedsSetup] = React.useState(false);
-  const [isLoading, setIsLoading] = React.useState(true);
+export function SetupForm() {
+  const { getFormProps, onSubmit, isSubmitting } = useForm();
 
-  React.useEffect(() => {
-    const ns = window.localStorage.getItem("needsSetup");
-
-    if (ns === "true") {
-      setNeedsSetup(true);
-    }
-
-    setIsLoading(false);
-  }, []);
-
-  return { needsSetup, isLoading };
-};
-
-export default function Setup() {
-  const { needsSetup, isLoading } = useNeedsSetup();
-
-  if (isLoading) {
-    return (
-      <Center style={{ height: "100vh" }}>
-        <Loader size="xl" variant="bars" />
-      </Center>
-    );
-  }
+  const handleSubmit = async (data) => {
+    await new Promise((r) => setTimeout(r, 2000));
+    console.log("Submitted Data", data);
+  };
 
   return (
-    <Container>
-      <Title order={1}>This is h1 title</Title>
-      <TextInput placeholder="Your name" label="Full name" required />
-      <Button>Submit</Button>
-    </Container>
+    <VStack
+      as="form"
+      spacing={5}
+      onSubmit={onSubmit(handleSubmit)}
+      {...getFormProps()}
+    >
+      <FormControl>
+        <FormLabel htmlFor="firstname">First Name</FormLabel>
+        <Input name="firstname" id="firstname" type="text" />
+      </FormControl>
+
+      <FormControl>
+        <FormLabel htmlFor="lastname">Last Name</FormLabel>
+        <Input name="lastname" id="lastname" type="text" />
+      </FormControl>
+
+      <FormControl>
+        <FormLabel htmlFor="email">Email address</FormLabel>
+        <Input name="email" id="email" type="email" />
+        <FormHelperText>It will be used as username.</FormHelperText>
+      </FormControl>
+
+      <FormControl>
+        <FormLabel htmlFor="password">Password</FormLabel>
+        <Input name="password" id="password" type="password" />
+      </FormControl>
+
+      <FormControl>
+        <FormLabel htmlFor="password_confirmation">Repeat Password</FormLabel>
+        <Input
+          name="password_confirmation"
+          id="password_confirmation"
+          type="password"
+        />
+      </FormControl>
+
+      <Button width="100%" type="submit" isLoading={isSubmitting}>
+        Start using Aurora!
+      </Button>
+    </VStack>
+  );
+}
+
+export function Setup() {
+  return (
+    <Flex minHeight="100vh" justifyContent="center" alignItems="center">
+      <Flex maxWidth="xl" direction="column" flex={1} gap={5}>
+        <Heading as="h1">Setup</Heading>
+        <SetupForm onSubmit={(data) => console.log(data)} />
+      </Flex>
+    </Flex>
   );
 }
