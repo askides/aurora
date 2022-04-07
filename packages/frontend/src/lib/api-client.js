cd1aa356049e130c66831486ecf4944fe3a50ba2
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-04-07 21:23:13 +0100

    fixed cors error for development

--- packages/frontend/src/lib/api-client.js
+++ packages/frontend/src/lib/api-client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const ApiClient = axios.create({
-  baseURL: "http://localhost:3000",
-  timeout: 1000,
+  // baseURL: "http://localhost:3000",
+  // timeout: 1000,
 });
