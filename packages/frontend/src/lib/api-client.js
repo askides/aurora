8883fdcac6dded84ee5936205e24c86de5e75ad4
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-04-10 13:29:27 +0100

    added bearer token to api-client

--- packages/frontend/src/lib/api-client.js
+++ packages/frontend/src/lib/api-client.js
@@ -3,4 +3,7 @@ import axios from "axios";
 export const ApiClient = axios.create({
   // baseURL: "http://localhost:3000",
   // timeout: 1000,
+  headers: {
+    authorization: `Bearer ${localStorage.getItem("aurora_access_token")}`,
+  },
 });
