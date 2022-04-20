e60e81261b06fc226412dd9b3702cc3fabc4866a
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-04-20 11:54:49 +0200

    moved to another folder

--- packages/backend/api/websites/[id]/index.js
+++ packages/backend/api/websites/[id]/index.js
@@ -1,6 +1,6 @@
-import { WebsiteController } from "../../lib/controllers/website-controller";
-import { authentication } from "../../lib/middleware/authentication";
-import { Router } from "../../lib/router";
+import { WebsiteController } from "../../../lib/controllers/website-controller";
+import { authentication } from "../../../lib/middleware/authentication";
+import { Router } from "../../../lib/router";
 
 export default async function handler(request, response) {
   const router = new Router(request, response);
