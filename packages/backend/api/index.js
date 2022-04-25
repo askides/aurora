bb886cb8bc6bd3598468803d88c2949edf1a0213
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-04-25 20:44:16 +0200

    refactored index

--- packages/backend/api/index.js
+++ packages/backend/api/index.js
@@ -1,11 +1,3 @@
-import { Router } from "../lib/router";
-
-export default async function handler(request, response) {
-  const router = new Router(request, response);
-
-  await router.route("GET", ({ res }) => {
-    return res.status(200).json({ message: "Aurora APIs are running!" });
-  });
-
-  router.fallback();
+export default async function handler(req, res) {
+  return res.status(200).json({ message: "Aurora APIs are running!" });
 }
