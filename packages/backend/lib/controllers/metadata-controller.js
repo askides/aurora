1740f2135018d72ea3fb9fcffdb96038b154ea19
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-04-26 11:49:04 +0200

    finalized metadata

--- packages/backend/lib/controllers/metadata-controller.js
+++ packages/backend/lib/controllers/metadata-controller.js
@@ -1,50 +1,51 @@
 import Joi from "joi";
 import * as AuroraDB from "../database";
-import { UnhauthorizedError } from "../error";
 import { authentication } from "../middleware/authentication";
 import { Controller } from "./controller";
 
 export class MetadataController extends Controller {
   constructor(request, response) {
     super(request, response);
-    this.middleware(authentication);
   }
 
   async index() {
-    const website = await AuroraDB.getWebsite(this.req.query.id);
+    const { id } = this.req.query;
+    const website = await AuroraDB.getWebsite(id);
 
-    // TODO: This route will be also public.
-    if (!website.is_public && website.user_id !== this.req.user.id) {
-      throw new UnhauthorizedError();
+    if (!website) {
+      this.abort(404);
+    }
+
+    if (!website.is_public) {
+      await authentication(this.req, this.res);
+
+      if (this.req.user.id !== website.user_id) {
+        this.abort(403);
+      }
     }
 
     const rules = Joi.object({
       start: Joi.string().required(),
       end: Joi.string().required(),
-      //unit: Joi.string().required().valid("hour", "day", "month", "year"),
     });
 
-    const validated = this.validate(this.req.query, rules);
+    this.validate(this.req.query, rules);
 
-    const { meta, ...filters } = req.query;
+    const { meta, ...filters } = this.req.query;
 
-    const data = await AuroraDB.getWebsiteViewsByMetadata(
-      this.req.query.id,
-      meta,
-      filters
-    );
+    const data = await AuroraDB.getWebsiteViewsByMetadata(id, meta, filters);
 
-    const unique = (el) => el.is_new_visitor;
+    const metadata = data.map((element) => {
+      const { value, events } = element;
+      const unique = events.filter((event) => event.is_new_visitor);
 
-    // TODO: check duplicated values
-    const mapped = data.map((el) => {
       return {
-        element: el.value,
-        views: el.events.length,
-        unique: el.events.filter(unique).length,
+        element: value,
+        views: events.length,
+        unique: unique.length,
       };
     });
 
-    return res.status(200).json(mapped);
+    return this.res.status(200).json(metadata);
   }
 }
