cb12c84d144fc2d06d8d9a156a39aee0d2f4fa9f
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-04-28 17:49:19 +0200

    a good initial implementation

--- packages/backend/lib/controllers/timeseries-controller.js
+++ packages/backend/lib/controllers/timeseries-controller.js
@@ -1,7 +1,45 @@
+import { addMinutes, eachDayOfInterval, eachHourOfInterval } from "date-fns";
 import Joi from "joi";
 import * as AuroraDB from "../database";
 import { authentication } from "../middleware/authentication";
 import { Controller } from "./controller";
+
+export function getTzOffset(timeZone, date = new Date()) {
+  const tz = date
+    .toLocaleString("en", {
+      timeZone,
+      timeStyle: "long",
+    })
+    .split(" ")
+    .slice(-1)[0];
+
+  const dateString = date.toString();
+
+  const offset =
+    Date.parse(`${dateString} UTC`) - Date.parse(`${dateString} ${tz}`);
+
+  // return UTC offset in minutes
+  return offset / 1000 / 60;
+}
+
+export function switchTz(date, tz) {
+  return addMinutes(date, getTzOffset(tz));
+}
+
+const interval = (startTs, endTs, unit, tz) => {
+  const start = new Date(Number(startTs));
+  const end = new Date(Number(endTs));
+
+  switch (unit) {
+    case "hour":
+      const int = eachHourOfInterval({ start, end });
+      return int.map((date) => switchTz(date, tz));
+    case "day":
+      return eachDayOfInterval({ start, end });
+    default:
+      throw new Error(`Invalid unit: ${unit}`);
+  }
+};
 
 export class TimeseriesController extends Controller {
   async index() {
@@ -36,18 +74,41 @@ export class TimeseriesController extends Controller {
       tz: this.req.query.tz || "UTC",
     };
 
-    const data = await AuroraDB.getWebsiteViewsTimeSeries(id, filters);
+    let data = await AuroraDB.getWebsiteViewsTimeSeries(id, filters);
 
-    const dropTime = (date) => {
-      return date.toISOString().split("T")[0];
+    // Convert the ts into an ISO string
+    data = data.map((row) => {
+      const isoDate = new Date(row.ts).toISOString();
+      return { ...row, ts: isoDate };
+    });
+
+    let int = interval(filters.start, filters.end, filters.unit, filters.tz);
+
+    // Convert the date into an ISO string
+    int = int.map((date) => date.toISOString());
+
+    const setTimeToZero = (date) => {
+      const [withoutTime] = date.split("T");
+      return `${withoutTime}T00:00:00.000Z`;
     };
 
-    const timeseries = data.map((element) => {
-      const date = dropTime(new Date(element.ts));
+    if (filters.unit !== "hour") {
+      data = data.map((row) => {
+        const dateWithoutTime = setTimeToZero(row.ts);
+        return { ...row, ts: dateWithoutTime };
+      });
+
+      int = int.map((date) => setTimeToZero(date));
+    }
+
+    const timeseries = int.map((element) => {
+      const views = data.find((row) => {
+        return row.ts === element;
+      });
 
       return {
-        timeseries: date,
-        count: element.count,
+        timeseries: element,
+        count: views ? views.count : 0,
       };
     });
 
