a001797052d7af43df62eabf2025a582931cecfe
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-04-29 14:55:55 +0200

    added preflight request to setup

--- packages/backend/api/setup.js
+++ packages/backend/api/setup.js
@@ -1,6 +1,16 @@
 import { SetupController } from "../lib/controllers/setup-controller";
 
-export default async function handler(request, response) {
+const withPreflight = (fn) => async (request, response) => {
+  if (request.method === "OPTIONS") {
+    return response.status(200).json({
+      body: "OK",
+    });
+  }
+
+  return await fn(request, response);
+};
+
+const handler = async (request, response) => {
   const setup = new SetupController(request, response);
 
   switch (request.method) {
@@ -11,4 +21,6 @@ export default async function handler(request, response) {
     default:
       return response.status(405).json({ message: "Method not allowed" });
   }
-}
+};
+
+export default withPreflight(handler);
