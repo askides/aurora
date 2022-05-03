1f6d4d6878b6fae06e5583dfb41cf2fcea899c19
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-05-03 20:37:47 +0200

    added flow for websites e2e

--- packages/frontend/cypress/integration/websites.spec.js
+++ packages/frontend/cypress/integration/websites.spec.js
@@ -1,11 +1,9 @@
 describe("Websites CRUD Flow", () => {
   beforeEach(() => {
-    // TODO: Obviouly, this is not the best way to do this.
-    cy.request("POST", "https://new-aurora-api.vercel.app/signin", {
+    cy.request("POST", Cypress.env("apiUrl") + "/signin", {
       email: "john.doe@example.com",
       password: "password",
     }).then((resp) => {
-      console.log(resp);
       window.localStorage.setItem("aurora_access_token", resp.body.accessToken);
     });
   });
@@ -15,10 +13,12 @@ describe("Websites CRUD Flow", () => {
     cy.get(".chakra-text").should("contain", "Here is absolute emptiness..");
   });
 
-  it("should create a website", () => {
+  it("should complete a full CRUD website flow", () => {
     cy.visit("/");
     cy.get(".chakra-text").should("contain", "Here is absolute emptiness..");
     cy.get("a").contains("Create New").click();
+
+    // Create
     cy.location().should((loc) => {
       expect(loc.pathname).to.eq("/websites/new");
     });
@@ -26,12 +26,40 @@ describe("Websites CRUD Flow", () => {
     cy.get("#name").type("My Website");
     cy.get("#url").type("https://example.com");
     cy.get("button").contains("Create").click();
+
+    // Index
     cy.location().should((loc) => {
       expect(loc.pathname).to.eq("/");
     });
 
-    // cy.get(".chakra-text")
-    //   .should("contain", "Here is absolute emptiness..")
-    //   .should("not.be.visible");
+    cy.get("table").contains("My Website").should("be.visible");
+    cy.get("table").contains("https://example.com").should("be.visible");
+    // cy.get("table").contains("PRIVATE").should("be.visible");
+    cy.get("table").contains("View Details").click();
+
+    // View & Edit // TODO: Checkbox is missing
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.match(/\/websites\/([a-z0-9]+)\/edit/);
+    });
+
+    cy.get("#name").should("have.value", "My Website");
+    cy.get("#url").should("have.value", "https://example.com");
+
+    cy.get("#name").clear().type("My Website 2");
+    cy.get("#url").clear().type("https://example.com/2");
+
+    cy.get("button").contains("Update").click();
+
+    cy.get("a").contains("Back").click();
+
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq("/");
+    });
+
+    // TODO: To be finished (we need to wait for SWR to update the data)
+    // cy.get("table").contains("View Details").click();
+
+    // cy.get("#name").should("have.value", "My Website 2");
+    // cy.get("#url").should("have.value", "https://example.com/2");
   });
 });
