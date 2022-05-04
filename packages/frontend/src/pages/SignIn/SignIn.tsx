3d49467042ce3363fc752e729bbf36262b54b12c
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-05-04 13:43:48 +0200

    improved signIn ui

--- packages/frontend/src/pages/SignIn/SignIn.tsx
+++ packages/frontend/src/pages/SignIn/SignIn.tsx
@@ -1,14 +1,24 @@
-import { Center, Flex, Heading } from "@chakra-ui/react";
+import { Center, Flex, Heading, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import { SignInForm } from "./SignInForm";
 
 const SignIn = () => {
   return (
     <Center height="100vh">
-      <Flex direction="column" alignItems="center" gap={10} width="md">
+      <Flex direction="column" alignItems="center" gap={8} width="md">
         <Heading as="h1" size="xl">
-          Please Sign In
+          Sign In
         </Heading>
         <SignInForm />
+
+        <Flex gap={1}>
+          <Text fontSize="base" color="gray.500">
+            First time here?
+          </Text>
+          <Text color="blue.500" fontWeight={500} as={Link} to="/setup">
+            Create the first user!
+          </Text>
+        </Flex>
       </Flex>
     </Center>
   );
