d224d2d0499c072741b79d95e67f6b30859ab684
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-05-09 10:35:38 +0200

    fix labels

--- packages/frontend/src/pages/Analytics/Charts/Stats.js
+++ packages/frontend/src/pages/Analytics/Charts/Stats.js
@@ -10,21 +10,20 @@ export function Stats({ filters }) {
       <SimpleGrid spacing={5} columns={{ sm: 1, md: 2, lg: 4 }}>
         <Stat label="Page Views" value={<Spinner />} />
         <Stat label="Unique Visitors" value={<Spinner />} />
-        <Stat label="Bounce Rate" value={<Spinner />} />
+        <Stat label="Bounces" value={<Spinner />} />
         <Stat label="Average Visit Time" value={<Spinner />} />
       </SimpleGrid>
     );
   }
 
+  const avg = Math.ceil(data.avgDuration / 1000);
+
   return (
     <SimpleGrid spacing={5} columns={{ sm: 1, md: 2, lg: 4 }}>
       <Stat label="Page Views" value={data.visits} />
       <Stat label="Unique Visitors" value={data.uniqueVisits} />
-      <Stat label="Bounce Rate" value={data.bounces} />
-      <Stat
-        label="Average Visit Time"
-        value={Math.ceil(data.avgDuration / 1000) + "s"}
-      />
+      <Stat label="Bounces" value={data.bounces} />
+      <Stat label="Average Visit Time" value={`${avg}s`} />
     </SimpleGrid>
   );
 }
